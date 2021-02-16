e976ca8bc3d49801b9443941138a8f2a2f840099
RyanLawWilson <59398576+RyanLawWilson@users.noreply.github.com>
2021-02-16 14:06:56 -0700

    Add Areas

---
+++ TheatreCMS3/Areas/Rental/RentalAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace TheatreCMS3.Areas.Rental
+{
+    public class RentalAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Rental";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Rental_default",
+                "Rental/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ TheatreCMS3/Models/Testing.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TheatreCMS3.Models
+{
+    public class Testing
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int asdf { get; set; }
+    }
+}
