2bc457620f78c6af47a0436760efded4caa5b0f5
Kevin McCrea <draynomonsoon@gmail.com>
2021-02-24 15:46:45 -0800

    Add ProductionPhoto model

---
+++ TheatreCMS3/Areas/Production/Controllers/ProductionPhotoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TheatreCMS3.Areas.Production.Controllers
+{
+    public class ProductionPhotoController : Controller
+    {
+        // GET: Production/ProductionPhoto
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
