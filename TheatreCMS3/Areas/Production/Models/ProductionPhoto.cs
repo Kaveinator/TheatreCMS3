c26041495d35296f7b519690a082ea5972aaa04a
Kevin McCrea <draynomonsoon@gmail.com>
2021-02-24 16:25:49 -0800

    Add model, views, controller

--- TheatreCMS3/Areas/Production/Models/ProductionPhoto.cs
+++ TheatreCMS3/Areas/Production/Models/ProductionPhoto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,7 @@ namespace TheatreCMS3.Areas.Production.Models
 {
     public class ProductionPhoto
     {
+        [Key]
         public int ProPhotoId { get; set; }
         //public Byte[] PhotoFile { get; set; }
         public string Title { get; set; }
