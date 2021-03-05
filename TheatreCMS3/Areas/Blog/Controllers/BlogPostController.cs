15f68ff44c8c09d459db64b7e785b35f5ccdba5a
Arnoldj2 <74551278+Arnoldj2@users.noreply.github.com>
2021-03-04 18:50:43 -0700

    Created BlogPost Model & CRUD Pages

---
+++ TheatreCMS3/Areas/Blog/Controllers/BlogPostController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TheatreCMS3.Areas.Blog.Data;
+using TheatreCMS3.Areas.Blog.Models;
+
+namespace TheatreCMS3.Areas.Blog.Controllers
+{
+    public class BlogPostController : Controller
+    {
+        BlogPostContext db = new BlogPostContext();
+
+
+
+        // GET: Blog/BlogPost
+        public ActionResult Index()
+        {
+
+            return View(db.BlogPosts.ToList());
+        }
+
+        // GET: Blog/BlogPost/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BlogPost blogPost = db.BlogPosts.Find(id);
+            if (blogPost == null)
+            {
+                return HttpNotFound();
+            }
+            return View(blogPost);
+        }
+
+        // GET: Blog/BlogPost/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Blog/BlogPost/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "BlogPostId,Title,Content,Posted,Author")]BlogPost blogPost)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.BlogPosts.Add(blogPost);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+            }
+            catch (DataException /* dex */)
+            {
+                //Log the error (uncomment dex variable name and add a line here to write a log.
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+            }
+            return View(blogPost);
+        }
+
+
+
+        // GET: Blog/BlogPost/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BlogPost blogPost = db.BlogPosts.Find(id);
+            if (blogPost == null)
+            {
+                return HttpNotFound();
+            }
+            return View(blogPost);
+        }
+
+        // POST: Blog/BlogPost/Edit/5
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditPost(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var blogpostToUpdate = db.BlogPosts.Find(id);
+            if (TryUpdateModel(blogpostToUpdate, "",
+               new string[] { "BlogPostId","Title","Content","Posted","Author" }))
+            {
+                try
+                {
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+                catch (DataException /* dex */)
+                {
+                    //Log the error (uncomment dex variable name and add a line here to write a log.
+                    ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
+                }
+            }
+            return View(blogpostToUpdate);
+        }
+
+        // GET: Blog/BlogPost/Delete/5
+        public ActionResult Delete(int? id, bool? saveChangesError = false)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ViewBag.ErrorMessage = "Delete failed. Try again, and if the problem persists see your system administrator.";
+            }
+            BlogPost blogPost= db.BlogPosts.Find(id);
+           
+
+
+            if (blogPost == null)
+            {
+                return HttpNotFound();
+            }
+            return View(blogPost);
+        }
+
+        // POST: Blog/BlogPost/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                BlogPost blogPost = db.BlogPosts.Find(id);
+                db.BlogPosts.Remove(blogPost);
+                db.SaveChanges();
+            }
+            catch (DataException/* dex */)
+            {
+                //Log the error (uncomment dex variable name and add a line here to write a log.
+                return RedirectToAction("Delete", new { id = id, saveChangesError = true });
+            }
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
