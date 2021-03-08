487bc5efd01c2607fe6b22f762747f236eabe4a3
Arnoldj2 <74551278+Arnoldj2@users.noreply.github.com>
2021-03-08 14:26:46 -0700

    Styling for Create and Edit pages

--- TheatreCMS3/Areas/Blog/Models/BlogPost.cs
+++ TheatreCMS3/Areas/Blog/Models/BlogPost.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -11,6 +12,8 @@ namespace TheatreCMS3.Areas.Blog.Models
         public int BlogPostId { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime Posted { get; set; }
         public string Author { get; set; }
     
