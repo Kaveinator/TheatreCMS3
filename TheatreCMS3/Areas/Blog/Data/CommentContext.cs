9ac09487fbdb7008bccfa26c9e47d52f7d7411d7
turtelneck <65428036+turtelneck@users.noreply.github.com>
2021-03-09 08:33:41 -0700

    Model created, applied to CRUD pages.

---
+++ TheatreCMS3/Areas/Blog/Data/CommentContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using TheatreCMS3.Areas.Blog.Models;
+
+namespace TheatreCMS3.Areas.Blog.Data
+{
+    public class CommentContext : DbContext
+    {
+        DbSet<Comment> Comments { get; set; }
+    }
+}
