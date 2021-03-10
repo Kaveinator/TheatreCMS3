4bac3b03330e748985d58fc8c8fdeeb2da722a71
Jaibean <58234317+Jaibean@users.noreply.github.com>
2021-03-09 21:31:22 -0500

    Added Rental request model to Rental Area, added the class to the dB context through the identity models then updated database to create the table in the dB

---
+++ TheatreCMS3/Areas/Rental/Models/RentalRequest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TheatreCMS3.Areas.Rental.Models
+{
+    [Table("RentalRequest")]
+    public class RentalRequest
+    {
+        [Key]
+        public int RentalRequestID { get; set; }
+        public string ContactPerson { get; set; }
+        public string Company { get; set; }
+        public DateTime RequestedTime { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string ProjectInfo { get; set; }
+        public int RentalCode { get; set; }
+        public bool Accepted { get; set; }
+        public bool ContractSigned { get; set; }
+    }
+}
