a30ef9ee7495a5d86e832d91d13f36da2d4bafc1
Jaibean <58234317+Jaibean@users.noreply.github.com>
2021-03-10 21:03:25 -0500

    changed date format for edit page warning

--- TheatreCMS3/Areas/Rental/Models/RentalRequest.cs
+++ TheatreCMS3/Areas/Rental/Models/RentalRequest.cs
@@ -15,13 +15,13 @@ namespace TheatreCMS3.Areas.Rental.Models
         public string ContactPerson { get; set; }
         public string Company { get; set; }
         [DataType(DataType.Date)]
-        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
         public DateTime RequestedTime { get; set; }
         [DataType(DataType.Date)]
-        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
         public DateTime StartTime { get; set; }
         [DataType(DataType.Date)]
-        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
         public DateTime EndTime { get; set; }
         public string ProjectInfo { get; set; }
         public int RentalCode { get; set; }
