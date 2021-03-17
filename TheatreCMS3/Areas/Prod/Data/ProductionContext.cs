8843f533d7302d0363a94494c9f40437e1b564d8
Dave Defourneaux <dave.defourneaux@gmail.com>
2021-03-17 17:26:39 -0400

    Revert "Remove previously created Context file."
    This reverts commit fa5d5656203506497327b4431abf281b2c0c8e20.

---
+++ TheatreCMS3/Areas/Prod/Data/ProductionContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using TheatreCMS3.Areas.Prod.Models;
+
+namespace TheatreCMS3.Areas.Prod.Data
+{
+    public class ProductionContext : DbContext
+    {
+        public ProductionContext() : base("ProductionContext")
+        {
+
+        }
+
+        public DbSet<Production> Productions { get; set; }
+    }
+}
