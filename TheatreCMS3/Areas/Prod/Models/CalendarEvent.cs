edf0fc7e5578da8cd360b003be98b1f688955d86
Brian Brown <bb.brown@mac.com>
2021-03-19 12:42:35 -0700

    BB-8491-CalendarModelCRUD push
    updated Enitity to 6.4.4 fixed datetime format annoations, uncommented out the footer

--- TheatreCMS3/Areas/Prod/Models/CalendarEvent.cs
+++ TheatreCMS3/Areas/Prod/Models/CalendarEvent.cs
@@ -14,16 +14,16 @@ namespace TheatreCMS3.Areas.Prod.Models
 		public string Title { get; set; }
 		public string Description { get; set; }
 		[DataType(DataType.Date)] // These annotations set the time and date formats
-		[DisplayFormat(DataFormatString ="{0:mm-dd-yyyy}", ApplyFormatInEditMode = true)]
+		[DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
 		public DateTime StartDate { get; set; }
 		[DataType(DataType.Date)]
-		[DisplayFormat(DataFormatString = "{0:mm-dd-yyyy}", ApplyFormatInEditMode = true)]
+		[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
 		public DateTime EndDate { get; set; }
 		[DataType(DataType.Time)]
-		[DisplayFormat(DataFormatString = "{0:hh:mm AM/PM}", ApplyFormatInEditMode = true)]
+		[DisplayFormat(DataFormatString = "{0:HH:mm}", ApplyFormatInEditMode = true)]
 		public DateTime StartTime { get; set; }
 		[DataType(DataType.Time)]
-		[DisplayFormat(DataFormatString = "{0:hh:mm AM/PM}", ApplyFormatInEditMode = true)]
+		[DisplayFormat(DataFormatString = "{0:HH:mm}", ApplyFormatInEditMode = true)]
 		public DateTime EndTime { get; set; }
 		public bool IsProduction { get; set; }
 		public int? ProductionId { get; set; }
