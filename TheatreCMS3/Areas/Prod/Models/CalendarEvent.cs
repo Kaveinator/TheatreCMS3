1e9634ada57f760eb700e37faef3c8d1158d4e72
Brian Brown <bb.brown@mac.com>
2021-03-23 17:57:10 -0700

    BB-8574-CalendarStylesPart2 push
    index display as shown in example, added dropdown for RUD options, styled the C(reate) button, all tested

--- TheatreCMS3/Areas/Prod/Models/CalendarEvent.cs
+++ TheatreCMS3/Areas/Prod/Models/CalendarEvent.cs
@@ -15,16 +15,16 @@ namespace TheatreCMS3.Areas.Prod.Models
 		public string Description { get; set; }
 		[DataType(DataType.Date)] // These annotations set the time and date formats
 		[DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-		public DateTime StartDate { get; set; }
+		public Nullable<DateTime> StartDate { get; set; }
 		[DataType(DataType.Date)]
 		[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-		public DateTime EndDate { get; set; }
+		public Nullable<DateTime> EndDate { get; set; }
 		[DataType(DataType.Time)]
-		[DisplayFormat(DataFormatString = "{0:HH:mm}", ApplyFormatInEditMode = true)]
-		public DateTime StartTime { get; set; }
+		[DisplayFormat(DataFormatString = "{0:h:mm}", ApplyFormatInEditMode = true)]
+		public Nullable<DateTime> StartTime { get; set; }
 		[DataType(DataType.Time)]
-		[DisplayFormat(DataFormatString = "{0:HH:mm}", ApplyFormatInEditMode = true)]
-		public DateTime EndTime { get; set; }
+		[DisplayFormat(DataFormatString = "{0:h:mm}", ApplyFormatInEditMode = true)]
+		public Nullable<DateTime> EndTime { get; set; }
 		public bool IsProduction { get; set; }
 		public int? ProductionId { get; set; }
 	}
