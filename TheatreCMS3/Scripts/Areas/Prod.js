5712e760def021d37b714a5857dbdc2a0c8b207d
Dave Defourneaux <dave.defourneaux@gmail.com>
2021-03-25 16:20:16 -0400

    Add JavaScript.

--- TheatreCMS3/Scripts/Areas/Prod.js
+++ TheatreCMS3/Scripts/Areas/Prod.js
@@ -1,1 +1,22 @@
-﻿
+﻿var ProductionDetailPostBackURL = '/Prod/Productions/DetailsModal';
+$(function () {
+    $("#").click(function () {
+        var $buttonClicked = $(this);
+        var id = $buttonClicked.attr('data-id');
+        $.ajax({
+            type: "GET",
+            url: ProductionDetailPostBackURL,
+            contentType: "application/json; charset=utf-8",
+            data: { "Id": id },
+            dataType: "json",
+            success: function (data) {
+                $('#ModalContent').html(data);
+                $('#DetailsModal').modal('show');
+
+            },
+            error: function () {
+                alert("Dynamic content load failed.");
+            }
+        });
+    });
+});
