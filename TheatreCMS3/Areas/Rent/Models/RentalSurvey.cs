842fedecd8d156ead7e43dd394e056b25036a45f
daisysalem <62805119+daisysalem@users.noreply.github.com>
2021-04-02 14:30:56 -0700

    Add CRUD

---
+++ TheatreCMS3/Areas/Rent/Models/RentalSurvey.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TheatreCMS3.Areas.Rent.Models
+{
+    public class RentalSurvey
+    {
+        [Key]
+        public int SurveyID { get; set; }
+        public int RecommendRentalRating { get; set; }
+        public int RentalCost { get; set; }
+        public int ExperienceRating { get; set; }
+        public String Feedback { get; set; }
+    }
+}
