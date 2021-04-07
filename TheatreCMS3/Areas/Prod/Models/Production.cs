cb5488d3f05608a996d598ae314cb6e3e6433797
Garrett-Hub <75766557+Garrett-Hub@users.noreply.github.com>
2021-04-07 16:09:59 -0600

    Create Production model and scaffolding

---
+++ TheatreCMS3/Areas/Prod/Models/Production.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TheatreCMS3.Areas.Prod.Models
+{
+    public class Production
+    {
+        [Key]
+        public int ProductionId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Playwright { get; set; }
+        public int Runtime { get; set; }
+        public DateTime OpeningDay { get; set; }
+        public DateTime ClosingDay { get; set; }
+        public DateTime ShowTimeEve { get; set; }
+        public DateTime? ShowTimeMat { get; set; }
+        public int Season { get; set; }
+        public bool IsWorldPremiere { get; set; }
+        public string TicketLink { get; set; }
+
+        public bool IsCurrentlyShowing()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
