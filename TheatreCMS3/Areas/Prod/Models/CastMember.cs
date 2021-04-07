625caf741c95daa26e705621932cb08d4ddf931b
DamonFlick <75501983+DamonFlick@users.noreply.github.com>
2021-04-07 12:44:00 -0700

    Create Cast Member Model

---
+++ TheatreCMS3/Areas/Prod/Models/CastMember.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TheatreCMS3.Areas.Prod.Models
+{
+    public class CastMember
+    {
+        [Key]
+        public int CastMemberID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int? YearJoined { get; set; }
+        [Required]
+        public PositionEnum MainRole { get; set; }
+        [Required]
+        public string Bio { get; set; }
+        [Required]
+        public byte[] Photo { get; set; }
+        [Required]
+        public bool CurrentMember { get; set; }
+        [Required]
+        public string Character { get; set; }
+        public int? CastYearLeft { get; set; }
+        public int? DebutYear { get; set; }
+
+    }
+    public enum PositionEnum
+    {
+        Actor,
+        Director,
+        Technitian,
+        StageManager,
+        Other
+    }
+}  
