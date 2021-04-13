e9eee862127e735e43c7876c87c3b04f84014536
libbybacon <libbybacon@gmail.com>
2021-04-13 11:30:23 -0400

    Created Comment model in Blog area.  Added Model DB Set to ApplicationDbContext definition

---
+++ TheatreCMS3/Areas/Blog/Models/Comment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using TheatreCMS3.Models;
+
+namespace TheatreCMS3.Areas.Blog.Models
+{
+    public class Comment
+    {
+        [Key]
+        public int CommentId { get; set; }
+        public ApplicationUser Author { get; set; }
+        public string Message { get; set; }
+        public DateTime CommentDate { get; set; }
+        public int Likes { get; set; }
+        public int Dislikes { get; set; }
+    }
+}
