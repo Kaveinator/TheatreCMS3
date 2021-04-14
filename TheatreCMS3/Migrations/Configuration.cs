9a2d49d5c6bb8566adec82b4e5e2d5a35390453d
Garrett-Hub <75766557+Garrett-Hub@users.noreply.github.com>
2021-04-14 12:45:01 -0600

    Seed database

--- TheatreCMS3/Migrations/Configuration.cs
+++ TheatreCMS3/Migrations/Configuration.cs
@@ -4,6 +4,7 @@
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using TheatreCMS3.Areas.Prod.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<TheatreCMS3.Models.ApplicationDbContext>
     {
@@ -15,10 +16,128 @@
 
         protected override void Seed(TheatreCMS3.Models.ApplicationDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            // Seed productions and photos then connect them properly
+            SeedProductions(context);
+            SeedProductionPhotos(context);
+            ConnectSeededProductionsWithPhotos();
+        }
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
-            //  to avoid creating duplicate seed data.
+        // Seeds productions to the database
+        private void SeedProductions(TheatreCMS3.Models.ApplicationDbContext context)
+        {
+            context.Productions.AddOrUpdate(x => x.ProductionId,
+                new Production
+                {
+                    ProductionId = 1,
+                    Title = "Hamilton",
+                    Description = "A fresh look at the era of the Founding Fathers.",
+                    Playwright = "Lin-Manuel Miranda",
+                    Runtime = 1,
+                    OpeningDay = DateTime.Today,
+                    ClosingDay = DateTime.Today,
+                    ShowTimeEve = DateTime.Now,
+                    ShowTimeMat = DateTime.Now,
+                    Season = 1,
+                    IsWorldPremiere = false,
+                    TicketLink = "tickets"
+                },
+
+                new Production
+                {
+                    ProductionId = 2,
+                    Title = "Wicked",
+                    Description = "Meet the witches of Oz before Dorothy dropped in.",
+                    Playwright = "Winnie Holzman",
+                    Runtime = 1,
+                    OpeningDay = DateTime.Today,
+                    ClosingDay = DateTime.Today,
+                    ShowTimeEve = DateTime.Now,
+                    ShowTimeMat = DateTime.Now,
+                    Season = 1,
+                    IsWorldPremiere = false,
+                    TicketLink = "tickets"
+                });
+        }
+
+        // Seeds some production photos to the database
+        private void SeedProductionPhotos(TheatreCMS3.Models.ApplicationDbContext context)
+        {
+            context.ProductionPhotos.AddOrUpdate(x => x.ProPhotoId,
+                new ProductionPhoto()
+                {
+                    ProPhotoId = 1,
+                    Title = "Hamilton",
+                    Description = "Photo Description",
+                    ProductionId = 1
+                },
+                new ProductionPhoto()
+                {
+                    ProPhotoId = 1,
+                    Title = "Hamilton 02",
+                    Description = "Photo Description",
+                    ProductionId = 1
+                },
+                new ProductionPhoto()
+                {
+                    ProPhotoId = 1,
+                    Title = "Hamilton 03",
+                    Description = "Photo Description",
+                    ProductionId = 1
+                },
+                new ProductionPhoto()
+                {
+                    ProPhotoId = 1,
+                    Title = "Hamilton 04",
+                    Description = "Photo Description",
+                    ProductionId = 1
+                },
+                new ProductionPhoto()
+                {
+                    ProPhotoId = 1,
+                    Title = "Hamilton 05",
+                    Description = "Photo Description",
+                    ProductionId = 1
+                },
+                new ProductionPhoto()
+                {
+                    ProPhotoId = 1,
+                    Title = "Wicked",
+                    Description = "Photo Description",
+                    ProductionId = 2
+                },
+                new ProductionPhoto()
+                {
+                    ProPhotoId = 1,
+                    Title = "Wicked 02",
+                    Description = "Photo Description",
+                    ProductionId = 2
+                },
+                new ProductionPhoto()
+                {
+                    ProPhotoId = 1,
+                    Title = "Hamilton 03",
+                    Description = "Photo Description",
+                    ProductionId = 2
+                },
+                new ProductionPhoto()
+                {
+                    ProPhotoId = 1,
+                    Title = "Hamilton 04",
+                    Description = "Photo Description",
+                    ProductionId = 2
+                },
+                new ProductionPhoto()
+                {
+                    ProPhotoId = 1,
+                    Title = "Hamilton 05",
+                    Description = "Photo Description",
+                    ProductionId = 2
+                });
+        }
+
+        private void ConnectSeededProductionsWithPhotos()
+        {
+
         }
     }
 }
