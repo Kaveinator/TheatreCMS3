0a9cc115054755acbb54c633f4e408b338a39a1c
rmccabe62 <rmccabe1023@gmail.com>
2021-04-15 16:19:44 -0400

    updated file

--- TheatreCMS3/Areas/Blog/Models/BlogAuthor.cs
+++ TheatreCMS3/Areas/Blog/Models/BlogAuthor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
@@ -9,7 +10,13 @@ namespace TheatreCMS3.Areas.Blog.Models
     {
         public int BlogAuthorid { get; set; }
         public string Name { get; set; }
+        public string Bio { get; set; }
         public DateTime Joined { get; set; }
         public DateTime Left { get; set; }
+    }
+
+    public class BlogAuthorDBContext : DbContext
+    {
+        public DbSet<BlogAuthor> BlogAuthors { get; set; } 
     }
 }
