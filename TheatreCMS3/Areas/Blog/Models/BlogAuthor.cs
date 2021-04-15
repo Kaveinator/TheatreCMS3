84f6cd46aa2c68c23cde29d11218012895f4a62b
rmccabe62 <rmccabe1023@gmail.com>
2021-04-15 16:03:10 -0400

    set props for blogauthor

---
+++ TheatreCMS3/Areas/Blog/Models/BlogAuthor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheatreCMS3.Areas.Blog.Models
+{
+    public class BlogAuthor
+    {
+        public int BlogAuthorid { get; set; }
+        public string Name { get; set; }
+        public DateTime Joined { get; set; }
+        public DateTime Left { get; set; }
+    }
+}
