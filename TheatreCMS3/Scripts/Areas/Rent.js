aba67bb580e7a9d5c2c394f9e0cb9e6bdd9043af
TBranscome <tbranscome11@gmail.com>
2021-04-21 23:22:00 -0400

    Rental History Create and Edit page style

--- TheatreCMS3/Scripts/Areas/Rent.js
+++ TheatreCMS3/Scripts/Areas/Rent.js
@@ -1,1 +1,15 @@
-﻿
+﻿//-----Rental History-----//
+var rentHistoryCheckBox = document.querySelector('.check-box');
+var rentHistoryNotesLabel = document.querySelector('.rentalHistory-notes-label');
+
+window.onload = isDamaged();
+function isDamaged() {
+    if (rentHistoryCheckBox.checked) {
+        rentHistoryNotesLabel.innerHTML = "Damages Incurred";
+    }
+    else {
+        rentHistoryNotesLabel.innerHTML = "Notes";
+    }
+    console.log("isDamaged() event fired");
+}
+
