aad32f30f9684bb39cb00c770fb74c2e8e6d9dfe
Chris Robinson <oops.ninja@outlook.com>
2021-04-23 09:59:10 -0700

    Seeking feedback.

--- TheatreCMS3/Areas/Rent/Models/RentalRequest.cs
+++ TheatreCMS3/Areas/Rent/Models/RentalRequest.cs
@@ -9,17 +9,28 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TheatreCMS3.Areas.Rent.Models
 {
-    public class RentalRequest 
-    { 
+    public class RentalRequest
+    {
+        [Key]
         public int RentalRequestID { get; set; }
         public string ContactPerson { get; set; }
         public string Company { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH\\:mm }", ApplyFormatInEditMode = true)]
         public DateTime RequestedTime { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH\\:mm }", ApplyFormatInEditMode = true)]
         public DateTime StartTime { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd  HH\\:mm}", ApplyFormatInEditMode = true)]
         public DateTime EndTime { get; set; }
+
+
         public string ProjectInfo { get; set; }
         public int RentalCode { get; set; }
         public bool Accepted { get; set; }
         public bool ContractSigned { get; set; }
+
     }
 }
