70b70a78b58dc54b6ac0bfb1e7ca52bc89f548cc
Breanna Starika <starikabreanna@gmail.com>
2021-04-27 17:33:21 -0600

    Added WordLimit Helper Method

---
+++ TheatreCMS3/Helpers/TextHelpers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheatreCMS3.Helpers
+{
+    public class TextHelpers 
+    {
+        public static string WordLimit(string str, int limit) //method will limit number of characters that are displayed using ellipses
+        {
+            if (str.Length > limit)
+            {
+                str = str.Substring(0, limit) + "...";
+            }
+
+            return str;
+        }
+    }
+}
