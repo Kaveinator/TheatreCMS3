6aadd7dda49670a4978577898746337ae3ca2227
Chris Robinson <oops.ninja@outlook.com>
2021-04-28 12:22:37 -0700

    Hoping for more feedback. Made a few changes, still having trouble. I also commented out The RequestedTime fields although im not sure that right either.

--- TheatreCMS3/Areas/Rent/Models/RentalRequest.cs
+++ TheatreCMS3/Areas/Rent/Models/RentalRequest.cs
@@ -21,51 +21,33 @@ namespace TheatreCMS3.Areas.Rent.Models
         [DataType(DataType.DateTime)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-ddTHH:mm}", ApplyFormatInEditMode = true)]
         public DateTime RequestedTime { get; set; }
-         
+
+
         public RentalRequest()
         {
-            RequestedTime = DateTime.UtcNow;
+            
+            RequestedTime = DateTime.UtcNow.ToUniversalTime();
         }
+        
 
         [DataType(DataType.DateTime)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-ddTHH:mm}", ApplyFormatInEditMode = true)]
         public DateTime StartTime { get; set; }
 
-        public string GetRentalDuration()
-        {
-            TimeSpan ts = DateTime.Now.ToUniversalTime().Subtract(StartTime);
-            int days = ts.Days;
-            int hours = ts.Hours;
-            int minutes = ts.Minutes;
-            int seconds = ts.Seconds;
-
-            if (days == 1)
-                return "Start Time";
-            if (days > 1)
-                return string.Format("{0} days ago", days);
-            if (hours > 0)
-                return string.Format("{0} hours ago", hours);
-            if (minutes > 0)
-                return string.Format("{0} minutes ago", minutes);
-            if (seconds > 0)
-                return string.Format("{0} seconds", seconds);
-            return "End Time";
-        }
+        //public static void GetRentalDuration()
+        //{
+            
+        //}
 
         [DataType(DataType.DateTime)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-ddTHH:mm}", ApplyFormatInEditMode = true)]
         public DateTime EndTime { get; set; }
 
-        //public string GetTimeRemaining()
+        //public static void GetTimeRemaining()
         //{
-        //    TimeSpan ts = DateTime.Now.ToUniversalTime().Subtract(EndTime);
-        //    int days = ts.Days;
-        //    int hours = ts.Hours;
-        //    int minutes = ts.Minutes;
-        //    int seconds = ts.Seconds;
+
         //}
-
-
+        
         public string ProjectInfo { get; set; }
         public int RentalCode { get; set; }
         public bool Accepted { get; set; }
