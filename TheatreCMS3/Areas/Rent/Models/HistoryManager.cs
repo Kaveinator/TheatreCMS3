9c97319fa8b3544115887878c7ab2d639f7ff5ed
TBranscome <tbranscome11@gmail.com>
2021-04-27 20:36:03 -0400

    Seed HistoryManager

--- TheatreCMS3/Areas/Rent/Models/HistoryManager.cs
+++ TheatreCMS3/Areas/Rent/Models/HistoryManager.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,5 +12,37 @@ namespace TheatreCMS3.Areas.Rent.Models
     {
         public int RestrictedUsers { get; set; }
         public int RentalReplacementRequests { get; set; }
+
+
+        public static void HistoryManagerSeed(ApplicationDbContext context)
+        {
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            if (!roleManager.RoleExists("HistoryManager"))
+            {
+                var role = new IdentityRole();
+                role.Name = "HistoryManager";
+                roleManager.Create(role);
+
+                
+                var historyManager = new HistoryManager
+                {
+                    UserName = "historyManager",
+                    Email = "historyManager@theatrevertigo.com",
+                    RestrictedUsers = 47,
+                    RentalReplacementRequests = 123
+                };
+                string historyManagerPass = "vertigoHistoryManager";
+
+                var checkHistoryMan = userManager.Create(historyManager, historyManagerPass);
+                
+                if (checkHistoryMan.Succeeded)
+                {
+                    userManager.AddToRole(historyManager.Id, "HistoryManager");
+                }
+
+            }
+        }
     }
 }
