145761fcea247c6279751dbc80a17e7157ebecb4
Priyanka Thakan <priyankawu@yahoo.com>
2021-04-29 14:34:03 -0700

    Table name fixed from ProductionPhotoes to ProductionPhotos. Needed to delete the database and 'update-database' again. All good now!

--- TheatreCMS3/Areas/Prod/Models/ProductionPhoto.cs
+++ TheatreCMS3/Areas/Prod/Models/ProductionPhoto.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace TheatreCMS3.Areas.Prod.Models
 {
+    //Data annotations: Data Annotations are attributes that can be applied to classes, properties, and/or methods and have syntax like [Key], [Display()], [Table()].
+    [Table("ProductionPhotos")]
     public class ProductionPhoto
     {
         [Key]       //Database needs a key column
