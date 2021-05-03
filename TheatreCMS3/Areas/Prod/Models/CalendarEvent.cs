902920d3118b80cf7af13e0c2ff7c068a8df1b90
jacobshinta <jacobshinta@me.com>
2021-05-03 18:36:56 -0400

    add calendar event

---
+++ TheatreCMS3/Areas/Prod/Models/CalendarEvent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace TheatreCMS3.Areas.Prod.Models
+{
+    public class CalendarEvent
+    {
+        [Key]
+        public int EventId { get; set; }
+        public string Title { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public DateTime? StartTime { get; set; }
+        public DateTime? EndTime { get; set; }
+        public bool AllDay { get; set; }
+        public int TicketsAvailable { get; set; }
+        public bool IsProduction { get; set; }
+    }
+}
