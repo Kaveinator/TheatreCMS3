816bbcff7359fb731bdfec1f8290e3d2535e13c4
Brian Reeves <1949commander@gmail.com>
2021-05-10 00:23:07 -0700

    Upload Code for BLog Photos
    Related Work Items: #8523

---
+++ TheatreCMS3/Areas/Blog/Models/BlogPhoto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TheatreCMS3.Areas.Blog.Models
+{
+    public class BlogPhoto
+    {
+        private byte photo;
+
+        [Key]
+        public int PK { get; set; }
+        public int BlogPhotoId { get; set; }
+        public string Title { get; set; }
+        public Byte Photo { get => photo; set => photo = value; }
+    }
+}
