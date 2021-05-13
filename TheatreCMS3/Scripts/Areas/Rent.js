2e2bccf1ac0019886eb64b08d53fa3329b51f861
esievaughn <esievaughn@gmail.com>
2021-05-13 09:44:12 -0600

    JavaScript to switch between "Notes" and "Damages Incurred" when checkbox is checked

--- TheatreCMS3/Scripts/Areas/Rent.js
+++ TheatreCMS3/Scripts/Areas/Rent.js
@@ -1,2 +1,13 @@
 ﻿
 /*---Rental History Checkbox/Label ---*/
+
+
+
+$("#chkDamagesIncurred").change(function () {
+    if (this.checked) {
+        $('#id1').show().html("Damages Incurred");
+    } else {
+        $('#id1').show().html("Notes");
+    }
+});
+
