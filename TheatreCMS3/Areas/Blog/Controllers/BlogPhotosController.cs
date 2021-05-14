bd970b7e6a477e5c716235eebfbc020ca2488b8c
Brian Reeves <1949commander@gmail.com>
2021-05-14 14:10:24 -0700

    Upload changes

--- TheatreCMS3/Areas/Blog/Controllers/BlogPhotosController.cs
+++ TheatreCMS3/Areas/Blog/Controllers/BlogPhotosController.cs
@@ -44,22 +44,6 @@ namespace TheatreCMS3.Areas.Blog.Controllers
             return View();
         }
 
-        //// This action handles the form POST and the upload
-        //[HttpPost]
-        //public ActionResult Create(HttpPostedFileBase file)
-        //{
-        //    // Verify that the user selected a file
-        //    if (file != null && file.ContentLength > 0)
-        //    {
-        //        // extract only the filename
-        //        var fileName = Path.GetFileName(file.FileName);
-        //        // store the file inside ~/App_Data/uploads folder
-        //        var path = Path.Combine(Server.MapPath("~/App_Data/uploads"), fileName);
-        //        file.SaveAs(path);
-        //    }
-        //    // redirect back to the index action to show the form once again
-        //    return RedirectToAction("Index");
-        //}
 
         // POST: Blog/BlogPhotos/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
