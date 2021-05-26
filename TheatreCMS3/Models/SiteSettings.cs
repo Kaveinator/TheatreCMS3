f88595359558bf71a03ec8acedbdd2385d5a9fdf
Alex Sanderson <asanderson1994s@gmail.com>
2021-05-26 15:14:34 -0700

    Story corrections

--- TheatreCMS3/Models/SiteSettings.cs
+++ TheatreCMS3/Models/SiteSettings.cs
@@ -4,17 +4,20 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Web;
+using System.Web.Hosting;
 
 namespace TheatreCMS3.Models
 {   
     public class SiteSettings
     {
-        public int number { get; set; }
+        public int copyright { get; set; }
 
         public static void ReadSiteSettings()
         {
+            //Grab the file path.
+            var filePath = HostingEnvironment.MapPath("~/SiteSettings.json");
             //Read the file into a string format and deserialize JSON to a type.
-            StreamReader r = new StreamReader(@"C:\Users\asand\source\repos\TheatreCMS3\TheatreCMS3\SiteSettings.json");
+            StreamReader r = new StreamReader(filePath);
             string jsonString = r.ReadToEnd();
             //Deserializes the JSON directly from the file.
             SiteSettings ss = JsonConvert.DeserializeObject<SiteSettings>(jsonString);
