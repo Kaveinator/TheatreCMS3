59877a2fea11550387123649e9e4224514082b1b
Alex Sanderson <asanderson1994s@gmail.com>
2021-05-31 09:38:42 -0700

    add PostMaster model

---
+++ TheatreCMS3/Areas/Blog/Models/PostMaster.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheatreCMS3.Areas.Blog.Models
+{
+    public class PostMaster : ApplicationUser
+    {
+        public int PublishedBlogPosts { get; set; }
+        public int RejectedBlogPosts { get; set; }
+        public int PendingBlogPosts { get; set; }
+    }
+}
