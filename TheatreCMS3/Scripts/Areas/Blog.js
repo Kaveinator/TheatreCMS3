931305fbc4df2ff12e761263b1999eaa6300eb96
GaryMatycich <garymatycich@gmail.com>
2021-05-31 12:36:45 -0600

    add basic javascript functionality

--- TheatreCMS3/Scripts/Areas/Blog.js
+++ TheatreCMS3/Scripts/Areas/Blog.js
@@ -1,1 +1,25 @@
-﻿
+﻿// Blog Author Section Start
+
+function btnAuthorDetails() {
+    var details = document.getElementById("ContentAuthorDetails");
+    var post = document.getElementById("ContentAuthorPost");
+    if (details.style.display === "none") {
+        details.style.display = "block";
+        document.getElementById('BtnAuthorDetails').style.backgroundColor = 'green';
+
+        post.style.display = "none";
+        document.getElementById('BtnAuthorPost').style.backgroundColor = '#4a4a4a';
+    }
+}
+
+function btnAuthorPost() {
+    var details = document.getElementById("ContentAuthorDetails");
+    var post = document.getElementById("ContentAuthorPost");
+    if (post.style.display === "none") {
+        post.style.display = "block";
+        document.getElementById('BtnAuthorPost').style.backgroundColor = 'green';
+
+        details.style.display = "none";
+        document.getElementById('BtnAuthorDetails').style.backgroundColor = '#4a4a4a';
+    }
+}
