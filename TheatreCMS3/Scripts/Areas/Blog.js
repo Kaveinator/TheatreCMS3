a1a96186aa4dbb01792b6bf7316f831fae5e91d7
GaryMatycich <garymatycich@gmail.com>
2021-06-01 15:10:22 -0600

    refactor menu javascript

--- TheatreCMS3/Scripts/Areas/Blog.js
+++ TheatreCMS3/Scripts/Areas/Blog.js
@@ -1,25 +1,52 @@
 ﻿// Blog Author Section Start
+var details = document.getElementById("ContentAuthorDetails");
+var detailsButton = document.getElementById("BtnAuthorDetails")
+var post = document.getElementById("ContentAuthorPost");
+var postButton = document.getElementById("BtnAuthorPost")
+var contact = document.getElementById("ContentAuthorContact")
+var contactButton = document.getElementById("BtnAuthorContact")
+var twitter = document.getElementById("ContentAuthorTwitter")
+var twitterButton = document.getElementById("BtnAuthorTwitter")
+
+function ResetAuthorMenu() {
+    details.style.display = "none";
+    post.style.display = "none";
+    contact.style.display = "none";
+    twitter.style.display = "none";
+    detailsButton.style.backgroundColor = '#4a4a4a';
+    postButton.style.backgroundColor = '#4a4a4a';
+    contactButton.style.backgroundColor = '#4a4a4a';
+    twitterButton.style.backgroundColor = '#4a4a4a';
+}
 
 function btnAuthorDetails() {
-    var details = document.getElementById("ContentAuthorDetails");
-    var post = document.getElementById("ContentAuthorPost");
     if (details.style.display === "none") {
+        ResetAuthorMenu();
         details.style.display = "block";
         document.getElementById('BtnAuthorDetails').style.backgroundColor = 'green';
-
-        post.style.display = "none";
-        document.getElementById('BtnAuthorPost').style.backgroundColor = '#4a4a4a';
     }
 }
 
 function btnAuthorPost() {
-    var details = document.getElementById("ContentAuthorDetails");
-    var post = document.getElementById("ContentAuthorPost");
     if (post.style.display === "none") {
+        ResetAuthorMenu();
         post.style.display = "block";
         document.getElementById('BtnAuthorPost').style.backgroundColor = 'green';
+    }
+}
 
-        details.style.display = "none";
-        document.getElementById('BtnAuthorDetails').style.backgroundColor = '#4a4a4a';
+function btnAuthorContact() {
+    if (contact.style.display === "none") {
+        ResetAuthorMenu();
+        contact.style.display = "block";
+        document.getElementById('BtnAuthorContact').style.backgroundColor = 'green';
+    }
+}
+
+function btnAuthorTwitter() {
+    if (twitter.style.display === "none") {
+        ResetAuthorMenu();
+        twitter.style.display = "block";
+        document.getElementById('BtnAuthorTwitter').style.backgroundColor = 'green';
     }
 }
