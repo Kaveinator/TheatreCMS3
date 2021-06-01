fcd4bab0883ce972b35cddc26fbbbec247650323
Nathan Skiles <nateskiles94@gmail.com>
2021-06-01 15:30:29 -0700

    Cleanup styling

--- TheatreCMS3/Areas/Blog/Controllers/CommentController.cs
+++ TheatreCMS3/Areas/Blog/Controllers/CommentController.cs
@@ -91,33 +91,6 @@ namespace TheatreCMS3.Areas.Blog.Controllers
             return View(comment);
         }
 
-        //// GET: Blog/Comment/Delete/5
-        //public ActionResult Delete(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    Comment comment = db.Comments.Find(id);
-        //    if (comment == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    return View(comment);
-        //}
-
-        //// POST: Blog/Comment/Delete/5
-        //[HttpPost, ActionName("Delete")]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult DeleteConfirmed(int id)
-        //{
-        //    Comment comment = db.Comments.Find(id);
-        //    db.Comments.Remove(comment);
-        //    db.SaveChanges();
-        //    return RedirectToAction("Index");
-        //}
-
-        // POST: Blog/Comment/Delete/
         [HttpPost]
         public async Task<JsonResult> Delete(int id)
 		{
