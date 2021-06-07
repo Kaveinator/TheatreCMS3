6526d4323d3eb6fc60c54ed0e618b01e93ce083e
cindy123jl <cindy123jl@gmail.com>
2021-06-07 09:36:21 -0700

    added the donation page

---
+++ TheatreCMS3/Models/Donate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheatreCMS3.Models
+{
+    public class Donate
+    {
+        public int DonateID { get; set; }
+        public int DonationID { get; set; }
+
+        public virtual Donation Donation { get; set; }
+    }
+}
