e3269d496a614174f46fd14f6ce8bf6288a817c9
cindy123jl <cindy123jl@gmail.com>
2021-06-11 10:29:04 -0700

    Changed format for the DateTIme

--- TheatreCMS3/Areas/Rent/Models/RentalRequest.cs
+++ TheatreCMS3/Areas/Rent/Models/RentalRequest.cs
@@ -15,15 +15,15 @@ namespace TheatreCMS3.Areas.Rent.Models
         public string Company { get; set; }
 
         [DataType(DataType.Time)]
-        [DisplayFormat(DataFormatString = "{0:HH:mm MM/dd/yyyy}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:HH:mm}", ApplyFormatInEditMode = true)]
         public DateTime? RequestedTime { get; set; }
 
         [DataType(DataType.Time)]
-        [DisplayFormat(DataFormatString = "{0:HH:mm MM/dd/yyyy}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:HH:mm}", ApplyFormatInEditMode = true)]
         public DateTime? StartTime { get; set; }
 
         [DataType(DataType.Time)]
-        [DisplayFormat(DataFormatString = "{0:HH:mm MM/dd/yyyy}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:HH:mm}", ApplyFormatInEditMode = true)]
         public DateTime? EndTime { get; set; }
 
         public string ProjectInfo { get; set; }
