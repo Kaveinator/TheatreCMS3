ac916c1d72d5a3db7c1b841d2ef032b14ba71c88
RyanLawWilson <59398576+RyanLawWilson@users.noreply.github.com>
2021-06-22 16:13:56 -0600

    Small fixes

--- TheatreCMS3/Models/IdentityModels.cs
+++ TheatreCMS3/Models/IdentityModels.cs
@@ -44,8 +44,6 @@ namespace TheatreCMS3.Models
 
         public System.Data.Entity.DbSet<TheatreCMS3.Areas.Prod.Models.CastMember> CastMembers { get; set; }
 
-        public System.Data.Entity.DbSet<TheatreCMS3.Areas.Rent.Models.RentalHistory> RentalHistories { get; set; }
-
         public System.Data.Entity.DbSet<TheatreCMS3.Areas.Blog.Models.BlogPost> BlogPosts { get; set; }
     }
 }
