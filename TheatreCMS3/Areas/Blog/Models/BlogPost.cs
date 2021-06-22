bbb498db9c2f7ee4a75572780ac0d25120124658
ashtn15kimsey <ashtn15@gmail.com>
2021-06-22 13:55:17 -0700

    update

--- TheatreCMS3/Areas/Blog/Models/BlogPost.cs
+++ TheatreCMS3/Areas/Blog/Models/BlogPost.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,10 @@ namespace TheatreCMS3.Areas.Blog.Models
         public int ID { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0: MM/dd/yyyy}")]
         public DateTime Posted { get; set; }
         public string Author { get; set; }
+        
     }
 }
