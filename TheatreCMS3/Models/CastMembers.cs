69d3d4f80273bed4a1366c6c683c2a9a06ccec7a
wespwojcik <67170488+wespwojcik@users.noreply.github.com>
2021-07-06 15:49:57 -0600

    CastMembers

---
+++ TheatreCMS3/Models/CastMembers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheatreCMS3.Models
+{
+    public class CastMembers
+    {
+        public int CastMemberId;
+        public string Name;
+        public int? YearJoined;
+        public PositionEnum MainRole;
+        public string Bio;
+        public Byte[] Photo;
+        public bool CurrenMember;
+        public string Character;
+        public int? CastYearLeft;
+        public int? DebutYear;
+
+    }
+
+    public enum PositionEnum
+    {
+        Actor,
+        Director,
+        Technician, 
+        StageManager,
+        Other
+    }
+}
