8182538cf5d8b55163b958a589a0403d67e2d218
Joshua Ramos <veliciquent@gmail.com>
2021-07-13 10:34:45 -0600

    Add TextHelpers class with sub method "Truncate"

---
+++ TheatreCMS3/Helpers/TextHelpers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheatreCMS3.Helpers
+{
+    public class TextHelpers
+    {
+        public static string Truncate(string text, int length)
+        {
+            if (string.IsNullOrEmpty(text)) return text; else if (text.Length <= length) return text;
+            while (text[length] == char.Parse(" ")) length -= 1;
+            return text.Substring(0, length) + "...";
+        }
+    }
+}
