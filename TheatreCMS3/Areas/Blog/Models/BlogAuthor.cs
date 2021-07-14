5240703e0951b34eed1999cee312462c8879c95f
Joshua Ramos <veliciquent@gmail.com>
2021-07-14 01:59:21 -0600

    Update Entity Framework to 6.4.4 and denote "BlogAuthorId" as Primary Key

--- TheatreCMS3/Areas/Blog/Models/BlogAuthor.cs
+++ TheatreCMS3/Areas/Blog/Models/BlogAuthor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,7 @@ namespace TheatreCMS3.Areas.Blog.Models
 {
     public class BlogAuthor
     {
+        [Key]
         public int BlogAuthorId { get; set; }
         public string Name { get; set; }
         public string Bio { get; set; }
