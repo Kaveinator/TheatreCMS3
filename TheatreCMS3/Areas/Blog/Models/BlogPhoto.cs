5245a0d10c2794c08e5c52ba15f8accb59692a41
pjamsa <Paul_Jamsa@msn.com>
2021-07-13 19:14:26 -0700

    Changed Entity Framewrok to 6.4.4 and denoted Primary Key

--- TheatreCMS3/Areas/Blog/Models/BlogPhoto.cs
+++ TheatreCMS3/Areas/Blog/Models/BlogPhoto.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace TheatreCMS3.Areas.Blog.Models
 {
     public class BlogPhoto
     {
+        [Key]
         public int BlogPhotoID { get; set; }
         public string Title { get; set; }
         //public byte[] Photo { get; set; }
