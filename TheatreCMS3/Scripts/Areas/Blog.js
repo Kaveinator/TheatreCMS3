96b625170a0450decbe31d1d85b24888bc63e303
CurleyT <curleyt727@gmail.com>
2021-07-18 20:58:47 -0400

    Completed Story 9481, on my end when launched it does not show changes to Blog.css and Blog.js, but it might show differently on your end? After inputting them into the DevConsole on Chrome, the current code all works as expected.

--- TheatreCMS3/Scripts/Areas/Blog.js
+++ TheatreCMS3/Scripts/Areas/Blog.js
@@ -1,1 +1,26 @@
-﻿
+﻿//Ajax function to Post to BlogPostController with the value taken from the Button creating the modal
+$("#BlogPost-index--deleteModal").on('show.bs.modal', function (event) {
+    let btn = $(event.relatedTarget);
+    let relatedTrgtBtnVal = btn.val();
+    let modal = $(this);
+    modal.find("#deleteButton").on("click", function (e) {
+        $.ajax({
+            type: "POST",
+            url: '@Url.Action("Delete")',
+            data: { Id: relatedTrgtBtnVal },
+            success: function () {
+                $(btn).closest("tr").hide();
+                $("#BlogPost-index--deleteModal").modal("hide");
+                $(".BlogPost-index--deleteNotif").fadeIn(500).delay(3000).fadeOut(2000);
+            },
+            error: function () {
+            }
+        });
+    });
+})
+
+//On Close Event for Modal that removes the listener from the DeleteButton that was applied above
+$("#BlogPost-index--deleteModal").on('hide.bs.modal', function () {
+    let modal = $(this);
+    modal.find("#deleteButton").off();
+})
