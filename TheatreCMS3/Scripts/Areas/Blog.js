33f448e3f27bfa1e373f8f64ad3d86e93d0dd85c
Joshua Ramos <veliciquent@gmail.com>
2021-07-20 06:55:32 -0600

    Update external JS

--- TheatreCMS3/Scripts/Areas/Blog.js
+++ TheatreCMS3/Scripts/Areas/Blog.js
@@ -1,19 +1,4 @@
 ﻿function getBlogBio(id) {
-    alert("Halp");
-    $(document).ready(function () {
-        var url = '/Blog/BlogAuthors/BlogBio/' + id;
-        $.get(url, null, function (data) {
-            $("#displayText" + id).html(data);
-        });
-    })
-}
-
-function getBlogPost(id) {
-    $(document).ready(function () {
-        $("#displayText" + id).html("Implmented in future stories");
-    })
-}
-function getBlogBio(id) {
     $(document).ready(function () {
         var url = '/Blog/BlogAuthors/BlogBio/' + id;
         $.get(url, null, function (data) {
