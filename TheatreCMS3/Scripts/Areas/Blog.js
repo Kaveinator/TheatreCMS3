7ac796b130884796b9dcdd6d608f69b236b076a1
pjamsa <Paul_Jamsa@msn.com>
2021-07-23 01:11:55 -0700

    Deleted Commented Out section

--- TheatreCMS3/Scripts/Areas/Blog.js
+++ TheatreCMS3/Scripts/Areas/Blog.js
@@ -1,20 +1,4 @@
 
-//function readURL(input) {
-//    if (input.files && input.files[0]) {
-//        var reader = new FileReader();
-
-//        reader.onload = function (e) {
-//            $('#BlogPhoto-Create--Input_Img').attr('src', e.target.result);
-//        }
-
-//        reader.readAsDataURL(input.files[0]);
-//    }
-//}
-
-//$("#inputFile").change(function () {
-//    readURL(this);
-//});
-
 ; (function ($) {
     function readURL(input) {
         var $prev = $('#BlogPhoto-Create--Input_Img');
