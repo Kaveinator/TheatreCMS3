97f081167de23ab1669ab70dbc59eb4402976887
pjamsa <Paul_Jamsa@msn.com>
2021-07-23 01:04:18 -0700

    Fixed Broken Image Icon

--- TheatreCMS3/Scripts/Areas/Blog.js
+++ TheatreCMS3/Scripts/Areas/Blog.js
@@ -1,15 +1,40 @@
-function readURL(input) {
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
 
-        reader.onload = function (e) {
-            $('#BlogPhoto-Create--Input_Img').attr('src', e.target.result);
+//function readURL(input) {
+//    if (input.files && input.files[0]) {
+//        var reader = new FileReader();
+
+//        reader.onload = function (e) {
+//            $('#BlogPhoto-Create--Input_Img').attr('src', e.target.result);
+//        }
+
+//        reader.readAsDataURL(input.files[0]);
+//    }
+//}
+
+//$("#inputFile").change(function () {
+//    readURL(this);
+//});
+
+; (function ($) {
+    function readURL(input) {
+        var $prev = $('#BlogPhoto-Create--Input_Img');
+
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $prev.attr('src', e.target.result);
+            }
+
+            reader.readAsDataURL(input.files[0]);
+
+            $prev.show();
+        } else {
+            $prev.hide();
         }
-
-        reader.readAsDataURL(input.files[0]);
     }
-}
 
-$("#inputFile").change(function () {
-    readURL(this);
-});
+    $('#inputFile').on('change', function () {
+        readURL(this);
+    });
+})(jQuery);
