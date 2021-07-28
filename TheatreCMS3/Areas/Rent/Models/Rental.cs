bb33834b9a5581dcbdfca39e0101c300171a21d6
Brittany Barnett <barnettbrittanym@gmail.com>
2021-07-28 18:46:26 -0400

    Added CRUD functionality to Rentals

---
+++ TheatreCMS3/Areas/Rent/Models/Rental.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheatreCMS3.Areas.Rent.Models
+{
+    public class Rental
+    {
+        public int RentalId { get; set; }
+        public string RentalName { get; set; }
+        public int RentalCost { get; set; }
+        public string FlawsAndDamages { get; set; }
+
+    }
+}
