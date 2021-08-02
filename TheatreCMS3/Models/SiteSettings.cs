5cd2be6d93e386fb982b7a2503c6e479796749d6
Jeremy Munshower <jdmunshower@gmail.com>
2021-08-02 14:17:09 -0700

    Created SiteSettings json and class

---
+++ TheatreCMS3/Models/SiteSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheatreCMS3.Models
+{
+    public  class SiteSettings : ApplicationDbContext
+    {
+        public int ReadSiteSettings { get; set; }
+    }
+}
