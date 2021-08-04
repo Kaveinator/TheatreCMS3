1ed401b0cd3c8085cb7ba2e325415ee2174b4aeb
Jeremy Munshower <jdmunshower@gmail.com>
2021-08-04 10:34:32 -0700

    Changed ReadSiteSettings ins class. deserialize json file

--- TheatreCMS3/Models/SiteSettings.cs
+++ TheatreCMS3/Models/SiteSettings.cs
@@ -1,21 +1,33 @@
 ﻿using System.IO;
-using System;
 using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
 
 namespace TheatreCMS3.Models
 {
     public class SiteSettings : ApplicationDbContext
     {
-        public int Copyright { get; set; }
+        public int ReadSiteSettings { get; set; }
+
+        public void Deserializer()
+        {
+            SiteSettings siteSettings1 = JsonConvert.DeserializeObject<SiteSettings>(File.ReadAllText(@"C:\Users\8 7\Desktop\LiveProject\TheatreCMS3\SiteSettings.json"));
+
+            using (StreamReader file = File.OpenText(@"C:\Users\8 7\Desktop\LiveProject\TheatreCMS3\SiteSettings.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                SiteSettings siteSettings = (SiteSettings)serializer.Deserialize(file, typeof(SiteSettings));
+            }
+            return;
+        }
+        
     }
 
-        public class ReadSiteSettings
-    {
-            public static void Main()
-            {
-                string fileName = "SiteSettings.json";
-                string jsonString = File.ReadAllText(fileName);
-                SiteSettings siteSettings = JsonConvert.DeserializeObject<SiteSettings>(jsonString);
-            }
-        }
+   
+
+
+
+
+
 }
+
