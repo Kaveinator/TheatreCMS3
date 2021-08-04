703900a80ea1b54c6961b0a16c72109fd1ffbefd
Ariel Rodriguez <69484552+rdrigezariel@users.noreply.github.com>
2021-08-04 19:32:52 -0400

    Update ReadSiteSettings method to return a SiteSettings object and create AddressDetails class to support street, city, state, and zip properties.

--- TheatreCMS3/Models/SiteSettings.cs
+++ TheatreCMS3/Models/SiteSettings.cs
@@ -9,11 +9,20 @@ namespace TheatreCMS3.Models
     public class SiteSettings : ApplicationDbContext
     {
         public int Copyright { get; set; }
+        public string PhoneNumber { get; set; }
+        public AddressDetails Address { get; set; }
 
-        public static void ReadSiteSettings()
+        public class AddressDetails
+        {
+            public string Street { get; set; }
+            public string CityStateZip { get; set; }
+        }
+
+        public static SiteSettings ReadSiteSettings()
         {
             string filename = HttpContext.Current.Server.MapPath("SiteSettings.json");
             SiteSettings siteSettings1 = JsonConvert.DeserializeObject<SiteSettings>(File.ReadAllText(filename));
+            return siteSettings1;
         }
         
     }
