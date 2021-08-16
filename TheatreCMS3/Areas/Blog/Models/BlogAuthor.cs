0b0d9e58e3908300dc94b199966c50d9f7c193ec
Tabitha Y <63029748+tabii238@users.noreply.github.com>
2021-08-16 13:17:06 -0700

    create and edit pages

--- TheatreCMS3/Areas/Blog/Models/BlogAuthor.cs
+++ TheatreCMS3/Areas/Blog/Models/BlogAuthor.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -9,13 +8,9 @@ namespace TheatreCMS3.Areas.Blog.Models
     public class BlogAuthor
     {
         public int BlogAuthorId { get; set; }
-        [DisplayName("Author:")]
         public string Name { get; set; }
-        [DisplayName("Bio:")]
         public string Bio { get; set; }
-        [DisplayName("Date Joined:")]
         public DateTime Joined { get; set; }
-        [DisplayName("Date Left:")]
-        public DateTime Left { get; set; }
+        public DateTime Left { get; set; } 
     }
 }
