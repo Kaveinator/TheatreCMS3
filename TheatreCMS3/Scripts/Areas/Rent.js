5640384a9c79996f1bb2f647ac676120c828dee1
caboosecodes <ricardolopez413@hotmail.com>
2021-08-19 12:39:23 -0700

    Style Create and Edit views for Rental History. Write javascript for Create and Edit views.

--- TheatreCMS3/Scripts/Areas/Rent.js
+++ TheatreCMS3/Scripts/Areas/Rent.js
@@ -1,6 +1,7 @@
 ﻿var label = document.getElementById("damages");
 var checkbox = document.getElementById("isDamaged");
 
+// changes label from Notes to Damages incurred on create page
 checkbox.addEventListener("change", function () {
     if (this.checked == true) {
         label.innerHTML = "Damages Incurred";
@@ -9,12 +10,14 @@ checkbox.addEventListener("change", function () {
     }
 });
 
-//function labelToggle() {
-//    if (label.checked == true) {
-//        label.innerHTML = "Damages Incurred";
-//    } else {
-//        label.innerHTML = "Notes";
-//    }
-//}
+// loads the proper label depending if the checkbox is already checked
+window.addEventListener("load", function () {
+    if (checkbox.checked == true) {
+        label.innerHTML = "Damages Incurred";
+    } else {
+        label.innerHTML = "Notes";
+    }
+});
+
 
 
