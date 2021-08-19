91bed7c6a63a541f76f1d68ddaab286466c348df
caboosecodes <ricardolopez413@hotmail.com>
2021-08-19 11:49:53 -0700

    pushing for review of checkbox

--- TheatreCMS3/Scripts/Areas/Rent.js
+++ TheatreCMS3/Scripts/Areas/Rent.js
@@ -1,1 +1,20 @@
-﻿
+﻿var label = document.getElementById("damages");
+var checkbox = document.getElementById("isDamaged");
+
+checkbox.addEventListener("change", function () {
+    if (this.checked == true) {
+        label.innerHTML = "Damages Incurred";
+    } else {
+        label.innerHTML = "Notes";
+    }
+});
+
+//function labelToggle() {
+//    if (label.checked == true) {
+//        label.innerHTML = "Damages Incurred";
+//    } else {
+//        label.innerHTML = "Notes";
+//    }
+//}
+
+
