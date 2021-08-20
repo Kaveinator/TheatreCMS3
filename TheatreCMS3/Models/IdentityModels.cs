f6eaac8dbfe680c0a14d9f615639c1955ef606fb
Tabitha Y <63029748+tabii238@users.noreply.github.com>
2021-08-20 12:13:07 -0700

    HeadAuthor model/CRUD pages

--- TheatreCMS3/Models/IdentityModels.cs
+++ TheatreCMS3/Models/IdentityModels.cs
@@ -47,5 +47,7 @@ namespace TheatreCMS3.Models
         public DbSet<RentalHistory> RentalHistories { get; set; }
         public System.Data.Entity.DbSet<TheatreCMS3.Areas.Blog.Models.BlogAuthor> BlogAuthors { get; set; }
         public System.Data.Entity.DbSet<TheatreCMS3.Areas.Blog.Models.BlogPhoto> BlogPhotoes { get; set; }
+
+        public System.Data.Entity.DbSet<TheatreCMS3.Areas.Blog.Models.HeadAuthor> ApplicationUsers { get; set; }
     }
 }
