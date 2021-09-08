a9b839926949e4b2b5c961f3781534bd22a60b97
joelmindset <82834522+joelmindset@users.noreply.github.com>
2021-09-08 14:03:37 -0600

    like and dislike ajax json

---
+++ TheatreCMS3/Scripts/Areas/Blog.js
@@ -0,0 +1,42 @@
+function clickAddLike(commentId) {
+    {
+        try {
+            
+            $.ajax({
+                url: 'GetAddLike',
+                type: "POST",
+                data: { id: commentId },
+                success: function (result) {
+                    $("#likeValue").text("Current Likes: " + result.Data);          
+                },
+                error: function (error) {
+                    alert(error);
+                }
+            });
+        }
+        catch (e) {
+            alert(e.message);
+        }
+    }
+}
+function clickAddDislike(commentId) {
+    {
+        try {
+
+            $.ajax({
+                url: 'GetAddDislike',
+                type: "POST",
+                data: { id: commentId },
+                success: function (result) {
+                    $("#dislikeValue").text("Current Dislikes: " + result.Data);
+                },
+                error: function (error) {
+                    alert(error);
+                }
+            });
+        }
+        catch (e) {
+            alert(e.message);
+        }
+    }
+}
