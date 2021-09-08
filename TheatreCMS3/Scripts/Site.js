51fe2c9fee44b00c37f57b918a07b1ad9a46dd4b
jasonwsvt <71233413+jasonwsvt@users.noreply.github.com>
2021-09-08 13:30:57 -0400

    Stylize create and edit page for Rental History.

--- TheatreCMS3/Scripts/Site.js
+++ TheatreCMS3/Scripts/Site.js
@@ -1,1 +1,17 @@
-﻿
+﻿//Rental History Section
+$("#RentalHistoryForm").ready(() => {
+	//Function to change the Damages Incurred label based on the value of the Rental Damaged checkbox.
+	var setDamagesIncurredLabel = () => {
+		const damaged = $("#RentalDamaged")[0].checked
+		$("#DamagesIncurred").parent().find("label").text(damaged ? "Damages Incurred" : "Notes")
+	}
+
+	//Set the label of the Rental Damaged checkbox
+	$("#RentalDamaged").parent().parent().find("label").text("Damaged?")
+
+	//Create an onchange event for the Rental Damaged checkbox
+	$("#RentalDamaged").change(function () { setDamagesIncurredLabel() })
+
+	setDamagesIncurredLabel()
+});
+//End Rental History Section
