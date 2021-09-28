7e870bd78d7685ba94986d84f9dc54a4956313db
Sean Hager <smhager87@gmail.com>
2021-09-28 15:33:45 -0400

    Commit JSON and C# Story changes

---
+++ TheatreCMS3/Models/SiteSettings.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace TheatreCMS3.Models
+{
+    public class CopyrightSetting
+    {
+        public int Copyright { get; set; }
+
+    }
+
+    public class Program
+    {
+        public static void ReadSiteSettings()
+        {
+            string fileName = "/SiteSettings.json";
+            string readJson = File.ReadAllText(fileName);
+            CopyrightSetting copyrightSetting = JsonConvert.DeserializeObject<CopyrightSetting>(readJson);
+
+            Console.WriteLine(copyrightSetting.Copyright);
+            Console.ReadLine();
+        }
+    }
+}
