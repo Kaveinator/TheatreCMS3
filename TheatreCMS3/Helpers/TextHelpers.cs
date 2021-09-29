1db37c76003b0ac366a9263e334584a2c4d54fa0
Sophia <sophia.e.jordan@gmail.com>
2021-09-29 13:33:27 -0400

    Add TextHelper - character limit helper method

---
+++ TheatreCMS3/Helpers/TextHelpers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheatreCMS3.Helpers
+{
+    public static class TextHelpers
+    {
+        public static string Truncate(this string value,
+            int maxLength,
+            bool addEllipsis = false)
+        {
+            // Check for valid string before attempting to truncate
+            if (string.IsNullOrEmpty(value)) return value;
+
+            // Proceed with truncating
+            var result = string.Empty;
+            if (value.Length > maxLength)
+            {
+                result = value.Substring(0, maxLength);
+                if (addEllipsis) result += "...";
+            }
+            else
+            {
+                result = value;
+            }
+
+            return result;
+        }
+          
+    }
+}
