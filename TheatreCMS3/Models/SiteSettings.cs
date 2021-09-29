d5374cb6da6086758a5bc1794104152c57573f26
Sean Hager <smhager87@gmail.com>
2021-09-28 20:10:16 -0400

    Add SiteSettings.cs code for JSON deserialization.

--- TheatreCMS3/Models/SiteSettings.cs
+++ TheatreCMS3/Models/SiteSettings.cs
@@ -1,25 +1,20 @@
 ﻿using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
 using System.IO;
+
 
 namespace TheatreCMS3.Models
 {
-    public class CopyrightSetting
+
+    public class SiteSettings
     {
         public int Copyright { get; set; }
 
-    }
-
-    public class Program
-    {
         public static void ReadSiteSettings()
         {
             string fileName = "/SiteSettings.json";
             string readJson = File.ReadAllText(fileName);
-            CopyrightSetting copyrightSetting = JsonConvert.DeserializeObject<CopyrightSetting>(readJson);
-
-            Console.WriteLine(copyrightSetting.Copyright);
+            SiteSettings siteSettings = JsonConvert.DeserializeObject<SiteSettings>(readJson);
             Console.ReadLine();
         }
     }
