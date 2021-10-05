a1643cddf7e30e0798565c8bf8f931bf7f2d040e
PANDABOMB-RoA <64565446+PANDABOMB-RoA@users.noreply.github.com>
2021-10-05 16:17:31 -0700

    Create TextHelpers.LimitWords Method

--- TheatreCMS3/Helpers/TextHelpers.cs
+++ TheatreCMS3/Helpers/TextHelpers.cs
@@ -7,26 +7,24 @@ namespace TheatreCMS3.Helpers
 {
     public static class TextHelpers
     {
-        public static string Truncate(this string value,
-            int maxLength)
+        public static string LimitWords(this string stringInput,
+            int wordLimit)
         {
             // Check for valid string before attempting to truncate
-            if (string.IsNullOrEmpty(value)) return value;
+            if (string.IsNullOrEmpty(stringInput)) return stringInput;
 
-            // Proceed with truncating
-            var result = string.Empty;
-            if (value.Length > maxLength)
+            while (stringInput.Contains("  "))                //This while loop reduces double spaces to single spaces so ghost words aren't counted.
+                stringInput = stringInput.Replace("  ", " ");
+
+            int countWords = stringInput.Split().Length; //This Split function counts the amount of spaces in the string and adds 1 to give a word count.
+
+            int spaceCount = 0;
+            for (int i = 0; i < wordLimit; i++) //Use a loop to find the location of the space in which the " " of the last word in the limit is.
             {
-                result = value.Substring(0, maxLength);
-                result += "...";
-            }
-            else
-            {
-                result = value;
+                spaceCount = stringInput.IndexOf(" ", spaceCount + 1);
             }
 
-            return result;
+            return stringInput.Substring(0, stringInput.IndexOf(" ", spaceCount))+"..."); //Returns substring ending at word limit and adds elipses at the end of string.
         }
-
     }
 }
