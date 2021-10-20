b49e23670547ea7548aebd8ea8845396a26d4779
Vinh Pham <viquph@gmail.com>
2021-10-20 12:55:13 -0400

    Created (fully defined) relationship between BlogAuthor and BlogPost models.

--- TheatreCMS3/Areas/Blog/Models/BlogPost.cs
+++ TheatreCMS3/Areas/Blog/Models/BlogPost.cs
@@ -11,6 +11,10 @@ namespace TheatreCMS3.Areas.Blog.Models
         public string Title { get; set; }
         public string Content { get; set; }
         public DateTime Posted { get; set; }
-        public string Author { get; set; }
+        //public string Author { get; set; }  // Commented out b/c of defined relationship below:
+
+        // Creating (fully defined) relationship between BlogAuthor and BlogPost models
+        public virtual int BlogAuthorID { get; set; }
+        public virtual BlogAuthor BlogAuthor { get; set; }
     }
 }
