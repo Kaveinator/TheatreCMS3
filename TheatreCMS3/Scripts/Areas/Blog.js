3a3a3833be9f9a658cb02c987ce3da9f9daf35e4
Ivan Fisher <87035839+Ivanfisher@users.noreply.github.com>
2021-10-21 21:40:50 -0400

    Final changes to Blog Author Index page

--- TheatreCMS3/Scripts/Areas/Blog.js
+++ TheatreCMS3/Scripts/Areas/Blog.js
@@ -1,29 +1,28 @@
-var pageIndex = 1;
-showPages(pageIndex);
 
-function currentPage(n) {
-    showPages(pageIndex = n);
-}
+
 // This function is for the Blog Author Details page. It provides the ability to switch between views.
 function showPages(n, blogId) {
     
-    if (blogId=== undefined) { blogId= 0; }
+    if (blogId === undefined) { return; }
+    
     var displaying = $("#" + blogId);
-    var blogPostDisplay = $("#blogPost(" + blogId+ ")");
-    var navDisplay1 = $("#pg1(" + n + ")");
-    var navDisplay2 = $("#pg2(" + n + ")");
-    if (n = 1) {
-        displaying.style.display = "block";
-        blogPostDisplay.style.display = "none";
-        navDisplay1.removeClass("btn-success");
-        if (!navDisplay1.hasClass("btn-secondary")) { navDisplay1.addClass("btn-secondary") };
+    var blogPostDisplay = $("#blogPost-" + blogId);
+    var navDisplay1 = $("#pg1-" + blogId);
+    var navDisplay2 = $("#pg2-" + blogId);
+    if (n == 1) {
+        navDisplay1.addClass("btn-success");
+        displaying.show();
+        blogPostDisplay.hide();
+        navDisplay2.removeClass("btn-success");
+        if (!navDisplay1.hasClass("btn-secondary")) { navDisplay1.addClass("btn-secondary"); }
     }
 
-    if (n = 2) {
-        displaying.style.display = "block";
-        blogPostDisplay.style.display = "none";
-        navDisplay2.removeClass("btn-success");
-        if (!navDisplay2.hasClass("btn-secondary")) { navDisplay2.addClass("btn-secondary") };
+    if (n == 2) {
+        navDisplay2.addClass("btn-success");
+        displaying.hide();
+        blogPostDisplay.show();
+        navDisplay1.removeClass("btn-success");
+        if (!navDisplay2.hasClass("btn-secondary")) { navDisplay2.addClass("btn-secondary"); }
     }
     
 }
