f24b653f7a9fc73161fbc79943b15929f074d169
pmarino92 <86321304+pmarino92@users.noreply.github.com>
2021-10-27 17:02:09 -0700

    Create Rental History Page

--- TheatreCMS3/Scripts/Areas/Rent.js
+++ TheatreCMS3/Scripts/Areas/Rent.js
@@ -1,1 +1,12 @@
-﻿
+﻿// Rental History Create
+var Counter = 0;
+function checkLink() {
+    Counter += 1;
+    if (Counter % 2 == 0) {
+        document.getElementById("Switch").innerHTML = "Notes"
+    }
+    else {
+        document.getElementById("Switch").innerHTML = "Damages"
+    }
+}
+
