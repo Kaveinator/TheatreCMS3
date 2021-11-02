e4d7185e82a3052fbb5df4762386bc7baa36f14f
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-11-02 10:35:27 -0700

    Create Production Photo model

---
+++ TheatreCMS3/Models/ProductionPhotoModels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheatreCMS3.Models
+{
+    public class ProductionPhotoModels
+    {
+        public int ProPhotoID { get; set; }
+        //public byte[] PhotoFile { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
