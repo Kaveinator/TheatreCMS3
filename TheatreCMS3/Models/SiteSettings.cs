1bf5a4874bd1b10adab04b7d1ae48b94b75eeae5
HebronShanko <hebronshanko@gmail.com>
2021-11-16 20:17:08 -0800

    Create SiteSettings.json File & C# Class

---
+++ TheatreCMS3/Models/SiteSettings.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace TheatreCMS3.Models
+{
+    public class SiteSettings
+    {
+        public int Copyright { get; set; }
+
+
+        public static void ReadSiteSettings()
+        {
+            using (StreamReader r = new StreamReader("file.json"))
+            {
+                string json = r.ReadToEnd();
+                SiteSettings items = JsonConvert.DeserializeObject<SiteSettings>(json);
+            }
+        }
+    }  
+}
