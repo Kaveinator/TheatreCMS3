14c9ef29d42f123cfa148a31e949e6a74df35fac
MtSoftDevSolutions <87912940+MtSoftDevSolutions@users.noreply.github.com>
2021-11-23 17:47:54 -0500

    Will put in a database and add CRUD functionality

--- TheatreCMS3/Models/IdentityModels.cs
+++ TheatreCMS3/Models/IdentityModels.cs
@@ -45,6 +45,6 @@ namespace TheatreCMS3.Models
         public DbSet<Areas.Rent.Models.Rental> Rentals { get; set; }
         public DbSet<Areas.Rent.Models.RentalRequest> RentalsRequests { get; set; }
 
-
+        public System.Data.Entity.DbSet<TheatreCMS3.Areas.Prod.Models.Production> Productions { get; set; }
     }
 }
