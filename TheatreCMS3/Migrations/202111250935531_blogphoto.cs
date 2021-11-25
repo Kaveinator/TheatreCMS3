29170885f979af41b23d0d84ec29da126ff77fff
Ivan Galas <39977309+CodingMikey@users.noreply.github.com>
2021-11-25 01:37:12 -0800

    Adding migration

---
+++ TheatreCMS3/Migrations/202111250935531_blogphoto.cs
@@ -0,0 +1,16 @@
+﻿namespace TheatreCMS3.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class blogphoto : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
