353591b7f8c6fa72485f3896e0762c5f317be8cd
MtSoftDevSolutions <87912940+MtSoftDevSolutions@users.noreply.github.com>
2021-11-26 16:55:52 -0500

    Will Add production photos CRUD

--- TheatreCMS3/Models/IdentityModels.cs
+++ TheatreCMS3/Models/IdentityModels.cs
@@ -48,5 +48,7 @@ namespace TheatreCMS3.Models
         public System.Data.Entity.DbSet<TheatreCMS3.Areas.Prod.Models.Production> Productions { get; set; }
 
         public System.Data.Entity.DbSet<TheatreCMS3.Areas.Prod.Models.CalendarEventModel> CalendarEventModels { get; set; }
+
+        public DbSet<TheatreCMS3.Areas.Prod.Models.ProductionPhoto>ProductionPhotos { get; set; }
     }
 }
