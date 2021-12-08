d2ae1e56685b8e050d0c8de8382206e81bc2a5db
kaly <kalimamadou@gmail.com>
2021-12-08 09:25:52 -0800

    - Add the hover toggel on the dropdown menu

--- TheatreCMS3/Scripts/Areas/Rent.js
+++ TheatreCMS3/Scripts/Areas/Rent.js
@@ -1,1 +1,10 @@
-﻿
+﻿// Toggel the drowpdown menu
+$('.dropdown').hover(function () {
+    $(this).children(".dropdown-menu").css({
+        "display": "block"
+    });
+}, function () {
+    $(this).children(".dropdown-menu").css({
+        "display": "none"
+    });
+});
