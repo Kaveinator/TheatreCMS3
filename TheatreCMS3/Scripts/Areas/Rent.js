991321e4f43db1beb19926f5fb3130021dbfc757
kaly <kalimamadou@gmail.com>
2021-12-08 21:55:18 -0800

    Implement the sorting functionality on the rental histories index page

--- TheatreCMS3/Scripts/Areas/Rent.js
+++ TheatreCMS3/Scripts/Areas/Rent.js
@@ -1,22 +1,45 @@
-﻿// get the variable for the label and checkbox
-var damage = document.querySelector("#RentalDamaged");
-var decs = document.querySelector("#RentalLabel");
-// Listener when checked or enchecked
-damage.addEventListener('change', function () {
-    if (this.checked) {
-        decs.innerHTML = "Damages Incurred";
-    }
-    else {
-        decs.innerHTML = "Notes";
-    }
-});
-// Toggel the drowpdown menu
-$('.dropdown').hover(function () {
-    $(this).children(".dropdown-menu").css({
-        "display": "block"
+﻿$(function () {
+
+    // get the variable for the label and checkbox
+    $('#RentalDamaged').change(function () {
+        if ($(this).is(":checked")) {
+            $("#RentalLabel").html("Damages Incurred");
+        }
+        else {
+            decs.innerHTML = "Notes";
+        }
     });
-}, function () {
-    $(this).children(".dropdown-menu").css({
-        "display": "none"
+
+    // Toggel the drowpdown menu
+    $('.dropdown').hover(function () {
+        $(this).children(".dropdown-menu").css({
+            "display": "block"
+        });
+    }, function () {
+        $(this).children(".dropdown-menu").css({
+            "display": "none"
+        });
+    });
+
+    // Display the rental history list 
+    function loadRentalHistoryList(val) {
+        $.ajax({
+            type: "POST",
+            url: 'ShowRentalHistoryList',
+            data: { sortOrder: val },
+            success: function (data) {
+                console.log("success");
+                $("#rentHistoryListArea").html(data);
+            }
+        });
+    }
+    // On Page Load
+    loadRentalHistoryList("damaged");
+    // when selected
+    $("#sortSelect").on('change', function () {
+        event.preventDefault();
+        // value of the selection
+        var val = this.value;
+        loadRentalHistoryList(val)
     });
 });
