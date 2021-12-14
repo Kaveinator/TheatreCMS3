2df80d9692fbe0007501425e8462cc6da4b633a5
Jesse Kuykendall <84601365+techyjesse@users.noreply.github.com>
2021-12-14 11:27:33 -0800

    Entity Rental Model and CRUD pages

--- TheatreCMS3/Models/IdentityModels.cs
+++ TheatreCMS3/Models/IdentityModels.cs
@@ -53,5 +53,8 @@ namespace TheatreCMS3.Models
         public DbSet<TheatreCMS3.Areas.Blog.Models.BlogAuthor> BlogAuthors { get; set; }
 
         public System.Data.Entity.DbSet<TheatreCMS3.Areas.Rent.Models.RentalHistory> RentalHistories { get; set; }
+
+        public DbSet<Areas.Rent.Models.Rental> Rentals { get; set; }
+
     }
 }
