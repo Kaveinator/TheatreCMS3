c6e5b490da18151231daf64dda49e279ebe56cdf
Jason <AarsZach@yahoo.com>
2021-12-28 12:15:09 -0700

    Add new model and controller for blog post. ---
    ---
    Add:
    - Model in the Blog area called BlogPost
    - Model includes:
     - ID
     - Title
     - Content
     - DateTime
     - Author
    - Controller for BlogPost (System auto made views)
    -  Views include:
     - Index
     - Create
     - Edit
     - Details
     - Delete

---
+++ TheatreCMS3/Areas/Blog/Models/BlogPost.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TheatreCMS3.Areas.Blog.Models
+{
+    public class BlogPost
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime Posted { get; set; }
+        public string Author { get; set; }
+    }
+    public class MovieDBContext : DbContext
+    {
+        public DbSet<BlogPost> BlogPosts { get; set; }
+    }
+}
--- TheatreCMS3/Models/IdentityModels.cs
+++ TheatreCMS3/Models/IdentityModels.cs
@@ -47,5 +47,7 @@ namespace TheatreCMS3.Models
         public DbSet<TheatreCMS3.Areas.Rent.Models.RentalRequest> RentalRequests { get; set; }
         public DbSet<TheatreCMS3.Areas.Prod.Models.CastMember> CastMembers { get; set; }
         public DbSet<TheatreCMS3.Areas.Prod.Models.Production> Productions { get; set; }
+
+        public System.Data.Entity.DbSet<TheatreCMS3.Areas.Blog.Models.BlogPost> BlogPosts { get; set; }
     }
 }
