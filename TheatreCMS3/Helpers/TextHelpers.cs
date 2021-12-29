f9d6390186e91ba428e48ceaa01b1f7330aca7ef
codhharris713 <83795096+codhharris713@users.noreply.github.com>
2021-12-29 13:18:59 -0800

    added method to truncate string and adding ellipsis

---
+++ TheatreCMS3/Helpers/TextHelpers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheatreCMS3.Helpers
+{
+    public class TextHelpers
+    {
+        public static string Truncate(string value,
+            int maxLength,
+            bool addEllipsis = false)
+        {
+            if (string.IsNullOrEmpty(value)) return value;
+
+            var result = string.Empty;
+            if (value.Length > maxLength)
+            {
+                result = value.Substring(0, maxLength);
+                if (addEllipsis) result += "..."; 
+            }
+            else
+            {
+                result = value;
+            }
+            return result;
+        
+        }
+
+    }
+}
