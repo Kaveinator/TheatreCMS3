b8bb7702b3402d7ee349c0c83cbabdad469dd811
DForeman <dlydellf@hotmail.com>
2021-12-29 19:22:33 -0600

    Creating "Rental" class & its 2 subs; adding model to DbSet

---
+++ TheatreCMS3/Areas/Rent/Models/Rental.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheatreCMS3.Areas.Rent.Models
+{
+    public class Rental
+    {
+        public int RentalId { get; set; }
+        public string RentalName { get; set; }
+        public int RentalCost { get; set; }
+        public string FlawsAndDamages { get; set; }
+        public byte Photo { get; set; } // this attribute is in the UML diagram for the Rental Area
+    }
+
+    public class RentalEquipment : Rental
+    {
+        public bool ChokingHazard { get; set; }
+        public bool SuffocationHazard { get; set; }
+        public int PurchasePrice { get; set; }
+    }
+
+    public class RentalRoom : Rental
+    {
+        public int RoomNUmber { get; set; }
+        public int SquareFootage { get; set; }
+        public int MaxOccupancy { get; set; }
+    }
+}
