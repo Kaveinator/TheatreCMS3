2eb5639a733a0072a759b7b3c80f9808acc891b4
Shay R <shalondra.davis@protonmail.com>
2021-12-31 18:09:57 -0800

    Create a Comment Model and scaffold CRUD pages for it.

---
+++ TheatreCMS3/Areas/Blog/Models/Comment.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TheatreCMS3.Areas.Blog.Models
+{
+    public class Comment
+    {
+        public int CommentId { get; set; }
+        public string Author { get; set; }
+        public string Message { get; set; }
+        public DateTime CommentDate { get; set; }
+        public int Likes { get; set; }
+        public int Dislikes { get; set; }
+       
+        public Comment()
+        {
+            CommentDate = DateTime.Now;
+        }
+
+        public double LikeRatio()
+        {
+            int totalVotes = Likes + Dislikes;
+            double ratio = Likes / Convert.ToDouble(totalVotes);
+            double votePercentage = ratio * 100;
+            return votePercentage;
+        }
+    }
+     
+
+
+}
