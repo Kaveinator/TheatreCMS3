16a3857427944e0610b91744335cf75cdc3da472
michaelmtz10 <m.mtz1088@gmail.com>
2022-01-06 16:55:21 -0800

    create page for rental history

--- TheatreCMS3/Scripts/Areas/Rent.js
+++ TheatreCMS3/Scripts/Areas/Rent.js
@@ -1,1 +1,11 @@
 ﻿
+$("#damaged").on("click",function () {
+
+    if (damaged.checked) {
+        ($("#trigger").text("Notes"));
+    }
+    else { $("#trigger").text("Damages Incurred");
+    }
+
+});
+
