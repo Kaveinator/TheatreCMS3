18b5e4108b8322542d0815098c3cdef76c10efd7
Whitatt <60752908+Whitatt@users.noreply.github.com>
2022-01-25 18:40:01 -0800

    gabe took a look

--- TheatreCMS3/Models/IdentityModels.cs
+++ TheatreCMS3/Models/IdentityModels.cs
@@ -51,5 +51,7 @@ namespace TheatreCMS3.Models
         public System.Data.Entity.DbSet<TheatreCMS3.Areas.Blog.Models.BlogPhoto> BlogPhotoes { get; set; }
 
         public System.Data.Entity.DbSet<TheatreCMS3.Areas.Rent.Models.Rental> Rentals { get; set; }
+
+        public System.Data.Entity.DbSet<TheatreCMS3.Areas.Rent.Models.Rentalrequest> Rentalrequests { get; set; }
     }
 }
