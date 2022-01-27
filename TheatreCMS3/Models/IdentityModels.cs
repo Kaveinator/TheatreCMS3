b8f61766cb4bb0e30af721bfb19596737fdd0287
tnye02 <tnye02@gmail.com>
2022-01-26 20:51:54 -0800

    Create entity model for CastMember with CRUD functionality.

--- TheatreCMS3/Models/IdentityModels.cs
+++ TheatreCMS3/Models/IdentityModels.cs
@@ -53,5 +53,7 @@ namespace TheatreCMS3.Models
         public System.Data.Entity.DbSet<TheatreCMS3.Areas.Rent.Models.Rental> Rentals { get; set; }
 
         public System.Data.Entity.DbSet<TheatreCMS3.Areas.Rent.Models.Rentalrequest> Rentalrequests { get; set; }
+
+        public System.Data.Entity.DbSet<TheatreCMS3.Models.CastMember> CastMembers { get; set; }
     }
 }
