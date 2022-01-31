4bd88fdda53387b0bf9c05a604dd1e40df0d709d
CrewsControlSolutions <92337423+CrewsControlSolutions@users.noreply.github.com>
2022-01-31 16:42:57 -0700

    Adds a method for truncating a string to the new TextHelpers class

--- TheatreCMS3/Controllers/HomeController.cs
+++ TheatreCMS3/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TheatreCMS3.Helpers;
 using TheatreCMS3.Models;
 
 
@@ -25,6 +26,14 @@ namespace TheatreCMS3.Controllers
         [HttpGet]
         public ActionResult Contact()
         {
+            //This section below is used for testing the Truncate method. Simply
+            //uncomment the below code to test the Truncate method. Make sure
+            //to put a breakpoint on this comment to debug by stepping 
+            //through each line of code.
+/*            string testSentence = "The sunset in that movie was beautiful.";
+            string modifiedSentence = testSentence.Truncate(12);
+            Console.WriteLine(modifiedSentence); //useful in theory but I have not figured out how to view the console output*/
+
             ViewBag.Message = "Your contact page.";
 
             return View();
---
+++ TheatreCMS3/Helpers/TextHelpers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheatreCMS3.Helpers
+{
+    public static class TextHelpers
+    {
+        //method that truncates a string to a specified length (inclusive)
+        public static string Truncate(this string @this, int maxLength)
+        {
+            //initialize the ellipsis
+            const string suffix = "...";
+
+            //check for if the string is null or if the string length is
+            //shorter than or equal to the desired truncation length
+            if (@this == null || @this.Length <= maxLength)
+            {
+                return @this;
+            }
+
+            //truncate the string and append the ellipsis
+            return @this.Substring(0, maxLength) + suffix;
+        }
+    }
+}
