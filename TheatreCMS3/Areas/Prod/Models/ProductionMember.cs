1cf2970098b2a20f79c904349deb8cef605cd414
TnTien <nguyen.tien25@gmail.com>
2022-02-18 01:02:06 -0700

    Add ProductionMember model

---
+++ TheatreCMS3/Areas/Prod/Models/ProductionMember.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheatreCMS3.Areas.Prod.Models
+{
+    public enum prodPosition
+    {
+        Actor, Director, Technician, StageManager, Other
+    }
+    public class ProductionMember
+    {
+        public int ProductionMemberId { get; set; }
+        public String Name { get; set; }
+        public int? YearJoined { get; set; }
+        public prodPosition MainRole { get; set; }
+        public String Bio { get; set; }
+        //public Byte[] Photo { get; set; }
+        public bool CurrentMember { get; set; }
+        public String Character { get; set; }
+        public int? CastYearLeft { get; set; }
+        public int? DebutYearLeft { get; set; }
+    }
+}
