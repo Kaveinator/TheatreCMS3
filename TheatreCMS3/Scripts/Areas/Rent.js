0d92e8c6ec3e420377cbf05cd0057822b68e7f0c
Spencer Merrill <canadianfaller7@gmail.com>
2022-03-02 02:05:33 -0700

    Made create and edit form

--- TheatreCMS3/Scripts/Areas/Rent.js
+++ TheatreCMS3/Scripts/Areas/Rent.js
@@ -1,1 +1,28 @@
-﻿
+﻿/*  ==========================================
+ CHANGE LABEL TEXT FROM NOTES TO DAMAGED INCURRED
+* ========================================== */
+//function changed() {
+//    let damagedIncurred = document.getElementsByClassName("check-box");
+//    let notes = document.getElementById("changeName");
+//    if (!damagedIncurred.checked) {
+//        notes.te = "Damaged Incurred";
+//    }
+//    else {
+//        notes.innerHTML = "Damaged Incurred";
+//    }
+//}
+
+function changedText() {
+    var damageIncurred = document.getElementById("RentalDamaged");
+    var text = document.getElementById("changeName");
+
+    if (damageIncurred.checked == true) {
+        text.innerHTML = "Damaged Incurred"
+    }
+    else {
+        text.innerHTML = "Damaged Incurred"
+    }
+
+}
+
+  
