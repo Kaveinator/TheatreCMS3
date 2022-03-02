596bc7fa767c3cc091e80d4c9a0028c48bda277c
Vicky Jones <vlj8008@gmail.com>
2022-03-02 18:56:17 -0500

    Gabe test

--- TheatreCMS3/Models/IdentityModels.cs
+++ TheatreCMS3/Models/IdentityModels.cs
@@ -58,5 +58,7 @@ namespace TheatreCMS3.Models
         public System.Data.Entity.DbSet<TheatreCMS3.Areas.Rent.Models.RentalHistory> RentalHistories { get; set; }
 
         public System.Data.Entity.DbSet<TheatreCMS3.Areas.Rent.Models.Rental> Rentals { get; set; }
+
+        public System.Data.Entity.DbSet<TheatreCMS3.Areas.Rent.Models.RentalItem> RentalItems { get; set; }
     }
 }
