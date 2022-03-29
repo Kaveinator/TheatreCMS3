e7d69e2aa9f58445677c67753e87203ed7550115
clcar23 <92751822+clcar23@users.noreply.github.com>
2022-03-29 15:45:34 -0600

    created Word Limit Method

--- TheatreCMS3/Helpers/TextHelper.cs
+++ TheatreCMS3/Helpers/TextHelper.cs
@@ -29,6 +29,65 @@ namespace TheatreCMS3.Helpers
             //converts character array back to string to be returned by the method
             string output = new string(ch);
             return output;
+
+
         }
+
+        // this method will limit the amount of characters in a string
+        public static string LimitCharacters(string text, int length)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return string.Empty;
+            }
+
+            if (text.Length <= length)
+            {
+                return text;
+            }
+            // looking for spaces in the beginning of text and omitting it
+            if (text.Contains("  " + text))
+            {
+                return text.TrimStart();
+            }
+            // and at the end as well
+            if (text.Contains(text + "  "))
+            {
+                return text.TrimEnd();
+            }
+            // changing any double spacing to single spaces
+            while (text.Contains("  "))
+            {
+                text = text.Replace("  ", " ");
+            }
+            // checking the text count, iterating through it, returning 
+            if (text.Count() >= length)
+            {
+                int x = 0;
+                for (int i = 0; i < length; i++)
+                {
+                    x = text.IndexOf("", x + 1);
+                }
+                return text.Substring(0, text.IndexOf("", x)) + "...";
+            }
+            else
+            {
+                return text;
+            }
+
+            //here we will look for any characters that we could shorten the sentence at
+            char[] delimiters = new char[] { ' ', '.', ',', ':', ';' };
+            int index = text.LastIndexOfAny(delimiters, length - 3);
+
+            if (index > (length / 2))
+            {
+                return text.Substring(0, index) + "...";
+            }
+            else
+            {
+                return text.Substring(0, length - 3) + "...";
+            }
+        }
+
     }
 }
