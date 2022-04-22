a8c571005978e4a5b6e5f7920b9c851a32c36e7e
NatreJuris <95266939+NatreJuris@users.noreply.github.com>
2022-04-21 18:16:05 -0700

    yay

---
+++ TheatreCMS3/Areas/Rent/Controllers/RentalItemController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TheatreCMS3.Areas.Rent.Controllers
+{
+    public class RentalItemController : Controller
+    {
+        // GET: Rent/RentalItem
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
