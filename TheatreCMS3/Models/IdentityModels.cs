d35c80882d7c77d93304d7aea7ff28f59eaaf756
tenchamp31 <72271944+tenchamp31@users.noreply.github.com>
2022-05-08 19:44:34 -0400

    Create TheatreMember and CRUD Pages

--- TheatreCMS3/Models/IdentityModels.cs
+++ TheatreCMS3/Models/IdentityModels.cs
@@ -45,6 +45,6 @@ namespace TheatreCMS3.Models
 
         public DbSet<TheatreCMS3.Areas.Prod.Models.CastMember> CastMembers { get; set; }
 
-        
+        public System.Data.Entity.DbSet<TheatreCMS3.Areas.Prod.Models.TheatreMember> TheatreMembers { get; set; }
     }
 }  
