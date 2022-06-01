7e8eb6eb69361fd26dc8cbe012101cf33b674be4
ErickLee85 <erickcrowne85@gmail.com>
2022-06-01 12:30:31 -0500

    Added Entity Model for Rental Item

--- TheatreCMS3/Models/IdentityModels.cs
+++ TheatreCMS3/Models/IdentityModels.cs
@@ -45,6 +45,8 @@ namespace TheatreCMS3.Models
         public DbSet<Areas.Prod.Models.ProductionMember> productionMembers { get; set; }
         public DbSet<Areas.Prod.Models.CastMember> CastMembers { get; set; }
 
+        public DbSet<Areas.Rent.Models.RentalItem> RentalItems { get; set; }
+
         object placeHolderVariable;
     }
 }  
