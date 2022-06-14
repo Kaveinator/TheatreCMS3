3d0becd6ad94d27e1f329137719e24acb62e586f
kimten88 <kimtendulkar88@gmail.com>
2022-06-14 11:35:17 -0700

    Create TextHelper Method, test on Index.cshtml

---
+++ TheatreCMS3/Helpers/TextHelpers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheatreCMS3.Helpers
+{
+    public static class TextHelpers    
+    {
+        public static string CharacterLimit(string Input, int Length)
+        {
+            if (Input.Length < Length) return Input;
+            int index = Input.IndexOf(' ', Length);
+            return Input.Substring(0, index) + "...";
+        }
+    }
+}
