d4260e07b35960670394149a2bb9d0959ff5099b
Ny <nyarce805@gmail.com>
2022-06-14 16:12:22 -0500

    Add Rental Photo model and base CRUD functionality

--- TheatreCMS3/Models/IdentityModels.cs
+++ TheatreCMS3/Models/IdentityModels.cs
@@ -42,6 +42,8 @@ namespace TheatreCMS3.Models
 
         public System.Data.Entity.DbSet<TheatreCMS3.Areas.Prod.Models.CastMember> CastMembers { get; set; }
 
+        public System.Data.Entity.DbSet<TheatreCMS3.Areas.Rent.Models.RentalPhoto> RentalPhotoes { get; set; }
+
 
         /* ▼ Put DbSet's for your models below ▼ */
 
