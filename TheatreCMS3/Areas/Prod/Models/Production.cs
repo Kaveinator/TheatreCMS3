e6ce81ab01532fc75670d4f2fc89596b6e95a55d
Heather Shultz <theheatherloop@gmail.com>
2022-07-16 20:51:45 -0500

    Unintentionly clicked on this branch, it's now forcing me to commit all 'changes' in this branch to continue to another.

---
+++ TheatreCMS3/Areas/Prod/Models/Production.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace TheatreCMS3.Areas.Prod.Models
+    {
+        public class Production
+        {
+            [Key]
+            public int ProductionId { get; set; }
+
+            [Required(ErrorMessage = "Title is required"), Display(Name = "Title", Prompt = "Title")]
+            public string Title { get; set; }
+
+            [Required(ErrorMessage = "Description is required"), Display(Name = "Description",Prompt = "Description")]
+            public string Description { get; set; } 
+
+            [Required(ErrorMessage = "PlayWright is required"), Display(Name = "Playwright", Prompt = "Playwright")]
+            public string PlayWright { get; set; }
+
+            [Required(ErrorMessage = "Runtime is required"), Display(Name = "Run time", Prompt = "Run time")]
+            public int Runtime { get; set; }
+
+            [Required(ErrorMessage = "Opening Day is required"), Display(Name = "Opening Day", Prompt = "Opening Day")]
+            public DateTime OpeningDay { get; set; }
+
+            [Required(ErrorMessage = "Closing Day is required"), Display(Name = "Closing Day", Prompt = "Closing Day")]
+            public DateTime ClosingDay { get; set; }
+
+            [DataType(DataType.Time)]
+            [DisplayFormat(DataFormatString = "{0:00}", ApplyFormatInEditMode = true)]
+            [Required(ErrorMessage = "Show Time is required"), Display(Name = "Show Time - Evening")]
+            public DateTime ShowTimeEve { get; set; }
+
+            [DataType(DataType.Time)]
+            [DisplayFormat(DataFormatString = "{0:00}", ApplyFormatInEditMode = true)]
+            [Display(Name = "Show Time - Matinee")]
+            public DateTime? ShowTimeMat { get; set; }
+
+            [Required(ErrorMessage = "Season is required"), Display(Name = "Season", Prompt = "Season")]
+        public int Season { get; set; }
+
+            [Required, Display(Name = "Check if this is a world premiere:")]
+            public bool IsWorldPremiere { get; set; }
+
+            [Required(ErrorMessage = "Ticket Link is required"), Display(Name = "Link to purchase tickets", Prompt = "Link")]
+            public string TicketLink { get; set; }
+
+            [Required, Display(Name = "Check if this production is currently showing:")]
+            public bool IsCurrentlyShowing { get; set; }
+        }
+}
