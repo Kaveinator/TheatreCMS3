aeb0244b6eb7803efeb83d586d1397e7235308db
Alan-Mitchell <94767396+Alan-Mitchell@users.noreply.github.com>
2022-08-19 22:19:26 -0500

    push

--- TheatreCMS3/Models/IdentityModels.cs
+++ TheatreCMS3/Models/IdentityModels.cs
@@ -44,6 +44,8 @@ namespace TheatreCMS3.Models
         public System.Data.Entity.DbSet<TheatreCMS3.Areas.Rent.Models.Rental> Rentals { get; set; }
         public System.Data.Entity.DbSet<TheatreCMS3.Areas.Prod.Models.ProductionPhoto> ProductionPhotos { get; set; }
 
+        public System.Data.Entity.DbSet<TheatreCMS3.Areas.Prod.Models.Production> Productions { get; set; }
+
 
 
     }
