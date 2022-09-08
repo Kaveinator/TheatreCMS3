ee9f6d927752d0c79767e6bfc4d47e0df774e7c3
Lawrence McCary <98392880+lmccary11@users.noreply.github.com>
2022-09-08 17:07:34 -0400

    Add truncating method to Helpers.

---
+++ TheatreCMS3/Helpers/TextHelpers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheatreCMS3.Helpers
+{
+    public class TextHelpers
+    {
+        public static string CharLimit(string content, int cutoff = 15)
+        {
+            if (content == null || content.Length < cutoff) return content;
+            return content.Substring(0, cutoff) + "...";
+        }
+    }
+}
