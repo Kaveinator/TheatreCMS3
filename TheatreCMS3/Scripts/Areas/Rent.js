7b41a7882993eec9114db74d9da3986eecca802e
Lawrence McCary <98392880+lmccary11@users.noreply.github.com>
2022-09-15 13:45:48 -0400

    Add functionality and formatting to Create and Edit pages for Rental History.

--- TheatreCMS3/Scripts/Areas/Rent.js
+++ TheatreCMS3/Scripts/Areas/Rent.js
@@ -1,1 +1,9 @@
-﻿
+﻿function myFunction() {
+    // Get the checkbox
+    var checkbox = document.getElementById("RentalDamaged");
+    // Get the output text
+    var text = document.getElementById("rental_history-create--alert");
+    if (checkbox.checked == true) {
+        text.innerHTML = "DamageIncurred"
+    } if (checkbox.checked != true) { text.innerHTML = "Notes" }
+}
