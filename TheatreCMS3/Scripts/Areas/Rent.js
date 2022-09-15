3e9df9861a388a8732787ba99113655c28907930
Lawrence McCary <98392880+lmccary11@users.noreply.github.com>
2022-09-15 16:51:10 -0400

    Update colors to reflect root.

--- TheatreCMS3/Scripts/Areas/Rent.js
+++ TheatreCMS3/Scripts/Areas/Rent.js
@@ -2,8 +2,8 @@
     // Get the checkbox
     var checkbox = document.getElementById("RentalDamaged");
     // Get the output text
-    var text = document.getElementById("rental_history-create--alert");
+    var text = document.getElementById("rental_history-edit--alert");
     if (checkbox.checked == true) {
         text.innerHTML = "DamageIncurred"
-    } if (checkbox.checked != true) { text.innerHTML = "Notes" }
-}
+    } if (checkbox.checked != true) { text.innerHTML = "Notes" } 
+} 
