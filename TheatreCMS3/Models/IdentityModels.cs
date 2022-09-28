6b7a3d5e2f7492e6b151dc6aa128d0d70afd0b62
Maher Dakdouk <mdakdouk2015@outlook.com>
2022-09-28 12:37:40 -0400

    Add production model with CRUD pages

--- TheatreCMS3/Models/IdentityModels.cs
+++ TheatreCMS3/Models/IdentityModels.cs
@@ -52,5 +52,6 @@ namespace TheatreCMS3.Models
         public System.Data.Entity.DbSet<TheatreCMS3.Areas.Rent.Models.Rental> Rentals { get; set; }
         public DbSet<TheatreCMS3.Areas.Rent.Models.RentalRequest> RentalRequest { get; set; }
         public DbSet<TheatreCMS3.Areas.Prod.Models.CastMember> CastMembers { get; set; }
+        public DbSet<TheatreCMS3.Areas.Prod.Models.Production> Productions { get; set; }
     }
 }  
