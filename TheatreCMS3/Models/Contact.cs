69f779af03a7f530b40356a76e285506dc6649d8
Waderjohnson <104044129+Waderjohnson@users.noreply.github.com>
2022-10-12 17:04:05 -0600

    Changes to UI layout and Completed Contact Us Page

---
+++ TheatreCMS3/Models/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TheatreCMS3.Models
+{
+    public partial class Contact
+    {
+        [DisplayName("First & Last Name *")]
+        public string FullName { get; set; }
+        public string EmailAddress { get; set; }
+        public string Subject { get; set; }
+        [DisplayName("Message *")]
+        public string Message { get; set; }
+    }
+}
