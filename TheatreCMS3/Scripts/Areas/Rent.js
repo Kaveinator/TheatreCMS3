e2da510f6997662656fb4c08c0ca1cd749d9444f
gwebb17 <gwebb1723@gmail.com>
2022-11-04 13:33:35 -0700

    Added dropdown with functionality to CREATE (done), EDIT still needs functionality /changes applied

--- TheatreCMS3/Scripts/Areas/Rent.js
+++ TheatreCMS3/Scripts/Areas/Rent.js
@@ -1,24 +1,35 @@
 ﻿function showRental() {
-    alert("showRental function hit");
+    /*alert("showRental function hit");*/
     $(".rental-create--rentalequipmentformcomponent").hide();     //$ = select("element" or ".classname" or "#idname")  .hide() = built in function to hide selected($) element
     $(".rental-create--rentalroomformcomponent").hide();
 }
 
 function showRentalEquipment() {
-    alert("showRentalEquipment function hit");
+    /*alert("showRentalEquipment function hit");*/   //test alerts to display when functions are hit
     $(".rental-create--rentalroomformcomponent").hide();
     
 }
 
 function showRentalRoom() {
-    alert("showRentalRoom function hit");
+    /*alert("showRentalRoom function hit");*/
     $(".rental-create--rentalequipmentformcomponent").hide();
 }
 
-function changeRentalHeading() {
-    alert("changeRentalHeading hit");
-    var rentalHeading = "Rental";
-    var location = document.getElementsById("rental-create--formheading");
-    location.innerHTML = "string";
-    /*document.getElementsByClassName("rental-create--formheading").innerHTML = rentalHeading;*/
+
+//Functions to change h4 at top of Form to match selected dropdown item
+function changeRentalHeading1() {
+    /*alert("changeRentalHeading hit");*/
+    var location = document.getElementById("rental-create--formheading");
+    location.innerHTML = "Rental";
+   /* document.getElementsByIdName("rental-create--formheading").innerHTML = rentalHeading;*  <--OLD WAY DOESNT WORK*/
+}
+
+function changeRentalHeading2() {
+    var location = document.getElementById("rental-create--formheading");
+    location.innerHTML = "Rental Equipment";
+}
+
+function changeRentalHeading3() {
+    var location = document.getElementById("rental-create--formheading");
+    location.innerHTML = "Rental Room";
 }
