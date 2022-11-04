50e017008db79b8c7aa2c53a64798ff9957244c1
gwebb17 <gwebb1723@gmail.com>
2022-11-04 12:53:57 -0700

    Added dropdown and funcitonality that hides undesired fields of form

--- TheatreCMS3/Scripts/Areas/Rent.js
+++ TheatreCMS3/Scripts/Areas/Rent.js
@@ -1,1 +1,24 @@
-﻿ 
+﻿function showRental() {
+    alert("showRental function hit");
+    $(".rental-create--rentalequipmentformcomponent").hide();     //$ = select("element" or ".classname" or "#idname")  .hide() = built in function to hide selected($) element
+    $(".rental-create--rentalroomformcomponent").hide();
+}
+
+function showRentalEquipment() {
+    alert("showRentalEquipment function hit");
+    $(".rental-create--rentalroomformcomponent").hide();
+    
+}
+
+function showRentalRoom() {
+    alert("showRentalRoom function hit");
+    $(".rental-create--rentalequipmentformcomponent").hide();
+}
+
+function changeRentalHeading() {
+    alert("changeRentalHeading hit");
+    var rentalHeading = "Rental";
+    var location = document.getElementsById("rental-create--formheading");
+    location.innerHTML = "string";
+    /*document.getElementsByClassName("rental-create--formheading").innerHTML = rentalHeading;*/
+}
