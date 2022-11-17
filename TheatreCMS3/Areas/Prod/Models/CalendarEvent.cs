a79fc5067ea4bd1772b5fd80a5d58bc6e30e35f4
Jamesmb7 <106565960+Jamesmb7@users.noreply.github.com>
2022-11-17 16:35:47 -0600

    Will add CSS Styling to Create and Edit CalendarEvent Pages.

--- TheatreCMS3/Areas/Prod/Models/CalendarEvent.cs
+++ TheatreCMS3/Areas/Prod/Models/CalendarEvent.cs
@@ -11,12 +11,19 @@ namespace TheatreCMS3.Areas.Prod.Models
         [Key]
         public int EventId { get; set; }
         public string Title { get; set; }
+        [Display(Name = "Start Date")]//Adds spacing to the name "StartDate"
         public DateTime StartDate { get; set; }
+        [Display(Name = "End Date")]
         public DateTime EndDate { get; set; }
+        [Display(Name = "Start Time")]
         public DateTime? StartTime { get; set; }
+        [Display(Name = "End Time")]
         public DateTime? EndTime { get; set; }
+        [Display(Name = "All Day")]
         public bool AllDay { get; set; }
+        [Display(Name = "Tickets Available")]
         public int TicketsAvailable { get; set; }
+        [Display(Name = "Is Production")]
         public bool IsProduction { get; set; }
     }
 }
