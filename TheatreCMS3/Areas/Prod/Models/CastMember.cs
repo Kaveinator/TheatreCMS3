5df312c45ca3a40c45c8701ebe5f23205aa81523
jmternes <68615887+jmternes@users.noreply.github.com>
2022-11-30 12:05:32 -0600

    Add CastMember model with CRUD functionality

---
+++ TheatreCMS3/Areas/Prod/Models/CastMember.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheatreCMS3.Areas.Prod.Models
+{
+    public class CastMember
+    {
+        public int CastMemberId { get; set; }
+        public string Name { get; set; }
+        public int? YearJoined { get; set; }
+        public PositionEnum MainRole { get; set; }
+        public string Bio { get; set; }
+        //public byte[] vs { get; set; } needs method to handle photo prop
+        public bool CurrentMember { get; set; }
+        public string Character { get; set; }
+        public int? CastYearLeft { get; set; }
+        public int? DebutYear { get; set; }
+    }
+
+    public enum PositionEnum
+    {
+        Actor,
+        Director,
+        Technician,
+        StageManager,
+        Other
+    }
+}
