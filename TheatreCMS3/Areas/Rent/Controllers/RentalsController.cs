d0efa4a5e780af204562a93cd8968abf81c1ee70
Samantha <samanthax0326@gmail.com>
2022-12-01 15:53:58 -0800

    Create functioning edit page with all inherited classes

--- TheatreCMS3/Areas/Rent/Controllers/RentalsController.cs
+++ TheatreCMS3/Areas/Rent/Controllers/RentalsController.cs
@@ -126,80 +126,128 @@ namespace TheatreCMS3.Areas.Rent.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(
-            [Bind(Include = "RentalId,RentalName,RentalCost,FlawsAndDamages")] Rental rental, bool? chokingHazard, bool? suffocationHazard, int? purchasePrice, int? roomNumber, int? squareFootage, int? maxOccupancy)
+        public ActionResult Edit([Bind(Include = "RentalId,RentalName,RentalCost,FlawsAndDamages")] Rental rental, int? PurchasePrice, bool? ChokingHazard, bool? SuffocationHazard,
+            int? RoomNumber, int? SquareFootage, int? MaxOccupancy)
         {
-            if (ModelState.IsValid)
+            if (PurchasePrice > 0)
             {
-                RentalEquipment previousEntryEq = (RentalEquipment)(TempData["previous"] == null ? db.Rentals.Find(rental.RentalId) :
-                    (RentalEquipment)TempData["previous"]);
 
-                RentalRoom previousEntryRm = (RentalRoom)(TempData["previous"] == null ? db.Rentals.Find(rental.RentalId) :
-                    (RentalEquipment)TempData["previous"]);
+                RentalEquipment rentalEquipment = new RentalEquipment();
+                rentalEquipment.RentalId = rental.RentalId;
 
-                Rental previousEntryR = (Rental)(TempData["previous"] == null ? db.Rentals.Find(rental.RentalId) :
-                    (RentalEquipment)TempData["previous"]);
+                rentalEquipment.RentalName = rental.RentalName;
+                rentalEquipment.RentalCost = rental.RentalCost;
+                rentalEquipment.FlawsAndDamages = rental.FlawsAndDamages;
+                rentalEquipment.ChokingHazard = Convert.ToBoolean(ChokingHazard);
+                rentalEquipment.SuffocationHazard = Convert.ToBoolean(SuffocationHazard);
+                rentalEquipment.PurchasePrice = Convert.ToInt32(PurchasePrice);
 
-                //,ChokingHazard,SuffocationHazard,PurchasePrice,RoomNumber,SquareFootage,MaxOccupancy"
-                //while(Convert.ToBoolean(ERentalType.RentalEquipment))
-
-
-                if (purchasePrice != null)
-                {
-                    var rentalEquipments = new RentalEquipment
-                    {
-                        RentalId = rental.RentalId,
-                        RentalName = rental.RentalName,
-                        RentalCost = rental.RentalCost,
-                        FlawsAndDamages = rental.FlawsAndDamages,
-                        ChokingHazard = Convert.ToBoolean(chokingHazard),
-                        SuffocationHazard = Convert.ToBoolean(suffocationHazard),
-                        PurchasePrice = Convert.ToInt32(purchasePrice)
-                        //SuffocationHazard = rentalEquipment.SuffocationHazard,
-                        //PurchasePrice = rentalEquipment.PurchasePrice
-                    };
-
-                    
-
-                    db.Entry(rentalEquipments).State = EntityState.Modified;
-                    db.SaveChanges();
-                    return RedirectToAction("Index");
-
-
-                }
-
-                if (roomNumber != null)
-                //while(Convert.ToBoolean(ERentalType.RentalRoom))
-                {
-                    var rentalRooms = new RentalRoom
-                    {
-                        RentalId = rental.RentalId,
-                        RentalName = rental.RentalName,
-                        RentalCost = rental.RentalCost,
-                        FlawsAndDamages = rental.FlawsAndDamages,
-                        RoomNumber = Convert.ToInt32(roomNumber),
-                        SquareFootage = Convert.ToInt32(squareFootage),
-                        MaxOccupancy = Convert.ToInt32(maxOccupancy)
-                    };
-
-                    db.Entry(rentalRooms).State = EntityState.Modified;
-                    db.SaveChanges();
-                    return RedirectToAction("Index");
-                }
-
-                if (roomNumber == null && purchasePrice == null)
-                {
-                    db.Entry(rental).State = EntityState.Modified;
-                    db.SaveChanges();
-                    return RedirectToAction("Index");
-                }
-
-                return View(rental);
-
+                db.Entry(rentalEquipment).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
             }
 
+            if (RoomNumber > 0)
+            {
+                RentalRoom rentalRoom = new RentalRoom();
+                rentalRoom.RentalId = rental.RentalId;
 
+                rentalRoom.RentalName = rental.RentalName;
+                rentalRoom.RentalCost = rental.RentalCost;
+                rentalRoom.FlawsAndDamages = rental.FlawsAndDamages;
+                rentalRoom.RoomNumber = Convert.ToInt32(RoomNumber);
+                rentalRoom.SquareFootage = Convert.ToInt32(SquareFootage);
+                rentalRoom.MaxOccupancy = Convert.ToInt32(MaxOccupancy);
+
+                db.Entry(rentalRoom).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            if (ModelState.IsValid)
+            {
+                db.Entry(rental).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(rental);
         }
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit(
+        //    [Bind(Include = "RentalId,RentalName,RentalCost,FlawsAndDamages,ChokingHazard,SuffocationHazard,PurchasePrice,RoomNumber,SquareFootage,MaxOccupancy")] Rental rental, RentalEquipment rentalEquipment, RentalRoom rentalRoom)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        //RentalEquipment previousEntryEq = (RentalEquipment)(TempData["previous"] ?? db.Rentals.Find(rental.RentalId));
+
+        //        //RentalRoom previousEntryRm = (RentalRoom)(TempData["previous"] ?? db.Rentals.Find(rental.RentalId));
+
+        //        //Rental previousEntryR = (Rental)(TempData["previous"] == null ? db.Rentals.Find(rental.RentalId) :
+        //        //    (Rental)TempData["previous"]);
+
+        //        //,ChokingHazard,SuffocationHazard,PurchasePrice,RoomNumber,SquareFootage,MaxOccupancy"   bool? chokingHazard, bool? suffocationHazard, int? purchasePrice, int? roomNumber, int? squareFootage, int? maxOccupancy
+        //        //while(Convert.ToBoolean(ERentalType.RentalEquipment))
+
+        //        if (rentalEquipment.PurchasePrice != null)
+        //        {
+        //            var rentalEquipment = new RentalEquipment
+        //            {
+        //                RentalId = rental.RentalId,
+        //                RentalName = rental.RentalName,
+        //                RentalCost = rental.RentalCost,
+        //                FlawsAndDamages = rental.FlawsAndDamages,
+        //                ChokingHazard = rentalEquipment.ChokingHazard,
+        //                //SuffocationHazard = Convert.ToBoolean(suffocationHazard),
+        //                //PurchasePrice = Convert.ToInt32(purchasePrice)
+        //                SuffocationHazard = rentalEquipment.SuffocationHazard,
+        //                PurchasePrice = rentalEquipment.PurchasePrice
+        //            };
+
+        //            //previousEntryEq.ChokingHazard = rentalEquipments.ChokingHazard;
+        //            //previousEntryEq.SuffocationHazard = rentalEquipments.SuffocationHazard;
+        //            //previousEntryEq.PurchasePrice = rentalEquipments.PurchasePrice;
+
+        //            db.Entry(rentalEquipment).State = EntityState.Modified;
+        //            db.SaveChanges();
+        //            return RedirectToAction("Index");
+
+
+        //        }
+
+        //        if (rentalRoom.RoomNumber != null)
+        //        //while(Convert.ToBoolean(ERentalType.RentalRoom))
+        //        {
+        //            var rentalRooms = new RentalRoom
+        //            {
+        //                RentalName = rental.RentalName,
+        //                RentalCost = rental.RentalCost,
+        //                FlawsAndDamages = rental.FlawsAndDamages,
+        //                RoomNumber = rentalRoom.RoomNumber,
+        //                SquareFootage = rentalRoom.SquareFootage,
+        //                MaxOccupancy = rentalRoom.MaxOccupancy
+        //            };
+
+        //            //previousEntryRm.RoomNumber = rentalRoom.RoomNumber;
+        //            //previousEntryRm.SquareFootage = rentalRoom.SquareFootage;
+        //            //previousEntryRm.MaxOccupancy = rentalRoom.MaxOccupancy;
+
+        //            db.Entry(rentalRooms).State = EntityState.Modified;
+        //            db.SaveChanges();
+        //            return RedirectToAction("Index");
+        //        }
+
+        //        if (rentalRoom.RoomNumber == null && rentalEquipment.PurchasePrice == null)
+        //        {
+        //            db.Entry(rental).State = EntityState.Modified;
+        //            db.SaveChanges();
+        //            return RedirectToAction("Index");
+        //        }
+        //    }
+
+        //    return View(rental);
+        //}
 
         // GET: Rent/Rentals/Delete/5
         public ActionResult Delete(int? id)
