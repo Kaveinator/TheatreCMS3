c2cdd5a89ef22536ca4135b5cf0462771cd53e2d
Joel Johnson <82834522+joelmindset@users.noreply.github.com>
2022-12-01 17:54:38 -0700

    Revert "commiting staged changes after merge conflict"
    This reverts commit 0ad103f8fb3735d377fda317d2537548c0684711, reversing
    changes made to d0efa4a5e780af204562a93cd8968abf81c1ee70.

--- TheatreCMS3/Scripts/Site.js
+++ TheatreCMS3/Scripts/Site.js
@@ -1,1 +1,8 @@
-﻿
+﻿//Can't figure out why this jQuery does not work. Have tried multiple iterations with classes, ids, and elements
+
+//$("#NumPersons").load(function () {
+//    $("p.home-signin--container").length
+//});
+
+
+document.getElementById("NumPersons").innerHTML = document.querySelectorAll('p').length;
