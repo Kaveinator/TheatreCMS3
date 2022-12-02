59df50fa4303ff46b381cd79008c42279ceba9d1
Joel Johnson <82834522+joelmindset@users.noreply.github.com>
2022-12-01 19:06:09 -0700

    Revert "Merged PR 8734: SN-15385-RentalCRUDInheritedClasses"
    This reverts commit aed4514463eeb800efc0aaf54616a308f4ffd404, reversing
    changes made to 346ad7d252684d16d808a6010b8acb3c8c112123.

--- TheatreCMS3/Areas/Rent/Models/Rental.cs
+++ TheatreCMS3/Areas/Rent/Models/Rental.cs
@@ -3,51 +3,31 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
-using System.Web.Mvc;
 
 namespace TheatreCMS3.Areas.Rent.Models
 {
     public class Rental
     {
-        [Display(Name = "ID")]
         public int RentalId { get; set; }
-        [Display(Name = "Name")]
         public string RentalName { get; set; }
-        [Display(Name = "Cost per hour")]
         public int RentalCost { get; set; }
-        [Display(Name = "Current flaws or damages")]
         public string FlawsAndDamages { get; set; }
 
-        public ERentalType RentalOptions { get; set; }
+        public virtual ICollection<RentalEquipment> RentalEquipment { get; set; }
+        public virtual ICollection<RentalRoom> RentalRooms { get; set; }
     }
 
     public class RentalEquipment : Rental
     {
-        [Display(Name = "Choking hazard")]
         public bool ChokingHazard { get; set; }
-        [Display(Name = "Sufocation hazard")]
         public bool SuffocationHazard { get; set; }
-        [Display(Name = "Purchase price")]
-        public int? PurchasePrice { get; set; }
+        public int PurchasePrice { get; set; }
     }
 
     public class RentalRoom : Rental
     {
-        [Display(Name = "Room number")]
-        public int? RoomNumber { get; set; }
-        [Display(Name = "Square footage of room")]
-        public int? SquareFootage { get; set; }
-        [Display(Name = "Maximum occupancy of room")]
-        public int? MaxOccupancy { get; set; }
+        public int RoomNumber { get; set; }
+        public int SquareFootage { get; set; }
+        public int MaxOccupancy { get; set; }
     }
-
-    public enum ERentalType
-    {
-        Rental,
-        //[Display(Name = "Rental Equipment")]
-        RentalEquipment,
-        //[Display(Name = "Rental Room")]
-        RentalRoom
-    }
-
 }
