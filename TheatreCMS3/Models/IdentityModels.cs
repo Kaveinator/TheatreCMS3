970a3ed4494873eaabfceb2dfa9c972d35fda4a9
Keith Moore <keithmoore13@gmail.com>
2022-12-05 14:52:08 -0700

    Creating entity model for CalendarEvents + scaffolding the CRUD pages

--- TheatreCMS3/Models/IdentityModels.cs
+++ TheatreCMS3/Models/IdentityModels.cs
@@ -55,5 +55,7 @@ namespace TheatreCMS3.Models
         public System.Data.Entity.DbSet<TheatreCMS3.Areas.Rent.Models.RentalHistory> RentalHistory { get; set; }
 
         public System.Data.Entity.DbSet<TheatreCMS3.Areas.Prod.Models.CastMember> CastMembers { get; set; }
+
+        public System.Data.Entity.DbSet<TheatreCMS3.Areas.Prod.Models.CalendarEvent> CalendarEvent { get; set; }
     }
 }
