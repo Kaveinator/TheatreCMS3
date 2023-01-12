092bf6cf77ea2dfd28ee0073a6f9b6883bb8ab56
natedorgan <natedorgandevops@gmail.com>
2023-01-12 12:09:53 -0800

    Will add and style RentalHistory Create and Edit pages

--- TheatreCMS3/Scripts/Areas/Rent.js
+++ TheatreCMS3/Scripts/Areas/Rent.js
@@ -1,1 +1,8 @@
-﻿
+﻿$("#RentalDamaged").click(function () {
+    if (this.checked) {
+        document.getElementById("SpotTheChange").innerHTML = "Damages Incurred";
+    }
+    else {
+        document.getElementById("SpotTheChange").innerHTML = "Notes";
+    }
+});
