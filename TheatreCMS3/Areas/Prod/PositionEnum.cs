8ab0e89e2f488c8c2c9c062c1a02d6638dc0adde
Heyitsspencer <heyitsspencerdavis@gmail.com>
2023-01-19 15:48:04 -0600

    Create PositionEnum.cs

---
+++ TheatreCMS3/Areas/Prod/PositionEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheatreCMS3.Areas.Prod
+{
+    public enum PositionEnum
+    {
+        Actor,
+        Director,
+        Technician,
+        StageManager,
+        Other
+    }
+}
