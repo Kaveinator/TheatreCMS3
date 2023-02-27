baca144e4176e6d9e380677ecb71df6423998d79
Zelena Palijo <zpali853@gmail.com>
2023-02-26 17:50:20 -0800

    Add function that changes Notes to Damages Incurred if the Rental is damaged

--- TheatreCMS3/Scripts/Areas/Rent.js
+++ TheatreCMS3/Scripts/Areas/Rent.js
@@ -1,1 +1,13 @@
-﻿
+﻿$(document).ready(function () {
+    // When checkbox is checked, show damages incurred section instead
+    $('#damaged-checkbox').change(function () {
+        if ($(this).is(':checked')) {
+            $('#damages-incurred').show();
+            $('#notes').hide();
+        }
+        else {
+            $('#damages-incurred').hide();
+            $('#notes').show();
+        }                            
+    });
+});
