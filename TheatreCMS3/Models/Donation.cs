fcef9904396c64c6afd1362a5cf68da840315c13
tremorsross11 <118317710+tremorsross11@users.noreply.github.com>
2023-03-07 15:06:38 -0800

    Created a Donation Page

---
+++ TheatreCMS3/Models/Donation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TheatreCMS3.Models;
+
+namespace TheatreCMS3.Models
+{
+    public class Donation
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public decimal Amount { get; set; }
+        public string Comment { get; set; }
+    }
+}
