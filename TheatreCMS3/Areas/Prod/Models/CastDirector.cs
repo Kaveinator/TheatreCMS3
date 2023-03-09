5b89242ce141bf7b897e30a3f9d3243e078e2ddb
Samantha Sargent <samjsargent07@gmail.com>
2023-03-09 14:37:53 -0800

    Add method to seed CastDirector

--- TheatreCMS3/Areas/Prod/Models/CastDirector.cs
+++ TheatreCMS3/Areas/Prod/Models/CastDirector.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using TheatreCMS3.Models;
 
 namespace TheatreCMS3.Areas.Prod.Models
@@ -10,5 +12,39 @@ namespace TheatreCMS3.Areas.Prod.Models
     {
         public int HiredCastMembers { get; set; }
         public int FiredCastMembers { get; set; }
+
+        public static void SeedCastDirector(ApplicationDbContext context)
+        {
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            if (!roleManager.RoleExists("CastDirector"))
+            {
+                var role = new IdentityRole();
+                role.Name = "CastDirector";
+                roleManager.Create(role);
+            }
+
+            var director = new CastDirector
+            {
+                UserName = "jamesgeorge",
+                Email = "jamesgeorge@gmail.com",
+                EmailConfirmed = true,
+                PhoneNumber = "555-555-5555",
+                PhoneNumberConfirmed = true,
+                TwoFactorEnabled = false,
+                LockoutEnabled = false,
+                AccessFailedCount = 0,
+                HiredCastMembers = 20,
+                FiredCastMembers = 5
+            };
+
+            var chkUser = UserManager.Create(director);
+
+            if (chkUser.Succeeded)
+            {
+                var result = UserManager.AddToRole(director.Id, "CastDirector");
+            }
+        }
     }
 }
