df67a2dc4cbcaf0e1c54ef071fa08276e8f049cf
Brandon Roman <116917326+flipsun@users.noreply.github.com>
2023-03-15 11:55:26 -0700

    Updating HeadAuthor Part 1: Added ApplicationUser Inheritance

--- TheatreCMS3/Areas/Blog/Models/HeadAuthor.cs
+++ TheatreCMS3/Areas/Blog/Models/HeadAuthor.cs
@@ -4,9 +4,10 @@ namespace TheatreCMS3.Areas.Blog.Models
     using System.Data.Entity;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Linq;
+    
 
 
-    public partial class HeadAuthor 
+    public class HeadAuthor : TheatreCMS3.Models.ApplicationUser
     {
         public int ViewsPerMonth { get; set; }
         public int AuthorsHired { get; set; }
