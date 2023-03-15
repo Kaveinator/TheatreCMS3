c4decd747efdd37891be50706c6089b50ffb8b32
Leonardo S Salazar <steven.salazar583@gmail.com>
2023-03-15 16:03:25 -0700

    For Gabe

--- TheatreCMS3/Scripts/Areas/Rent.js
+++ TheatreCMS3/Scripts/Areas/Rent.js
@@ -1,1 +1,10 @@
-﻿
+﻿////function tableIcon() {
+////    var icon = document.getElementById("icon");
+////    var iconType = document.getElementsByTagName("i");
+
+////    if (typeof icon == "boolean") {
+////        if (icon == true) {
+////            iconType = 
+////        }
+////    }
+////}
