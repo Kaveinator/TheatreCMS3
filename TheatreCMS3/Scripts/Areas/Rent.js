cbc915b568f588f614708f24145dc1205b7d0fa6
Leonardo S Salazar <steven.salazar583@gmail.com>
2023-03-16 15:42:16 -0700

    The create and edit pages will contain an edited dynamic form that matches the main theme.

--- TheatreCMS3/Scripts/Areas/Rent.js
+++ TheatreCMS3/Scripts/Areas/Rent.js
@@ -1,10 +1,12 @@
-﻿////function tableIcon() {
-////    var icon = document.getElementById("icon");
-////    var iconType = document.getElementsByTagName("i");
+﻿// Function to change the label on the Create and Edit Form depending on checkbox
+function notesToDamage(thecheckbox, thelabel) {
 
-////    if (typeof icon == "boolean") {
-////        if (icon == true) {
-////            iconType = 
-////        }
-////    }
-////}
+    var checkbox = document.getElementById(thecheckbox);
+    var label = document.getElementById(thelabel);
+    if (!checkbox.checked) {
+        label.innerHTML = "Notes";
+    }
+    else {
+        label.innerHTML = "Damages Incurred";
+    }
+}
