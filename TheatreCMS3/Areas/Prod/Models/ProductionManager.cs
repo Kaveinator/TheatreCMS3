e14bb9bc28272cade93ae97a9795b5f5782b1412
chasetmartin <36861079+chasetmartin@users.noreply.github.com>
2023-03-27 08:30:47 -0700

    Adds a seed method to the ProductionManager model to instantiate an instance of a ProductionManager for testing purposes.

--- TheatreCMS3/Areas/Prod/Models/ProductionManager.cs
+++ TheatreCMS3/Areas/Prod/Models/ProductionManager.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using TheatreCMS3.Models;
 
 namespace TheatreCMS3.Areas.Prod.Models
@@ -11,5 +13,31 @@ namespace TheatreCMS3.Areas.Prod.Models
         public ProductionManager() : base() { }
         public string Title { get; set; }
         public DateTime ManagerStartDate { get; set; }
+        public static void SeedProductionManager(ApplicationDbContext ProductionManagerContext)
+        {
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(ProductionManagerContext));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(ProductionManagerContext));
+
+            // Creating a ProductionManager role
+            var role = new IdentityRole();
+            role.Name = "ProductionManager";
+            roleManager.Create(role);
+
+            // Instantiating an object of ProductionManager / Seeding object
+            var productionManager = new ProductionManager
+            {
+                UserName = "username",
+                Email = "email@email.com",
+                Title = "ProductionManagerTestSeed",
+                ManagerStartDate = DateTime.Now
+            };
+
+            var productionManagerPassword = "password";
+            var manager = userManager.Create(productionManager, productionManagerPassword);
+            if (manager.Succeeded)
+            {
+                userManager.AddToRole(productionManager.Id, "ProductionManager");
+            }
+        }
     }
 }
