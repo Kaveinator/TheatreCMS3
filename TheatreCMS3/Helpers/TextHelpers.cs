2ec71a4f0cdb881758bed6d90dfd11cf641599a5
meganwinkie95 <66391136+meganwinkie95@users.noreply.github.com>
2023-03-30 12:46:35 -0700

    Reserved Story

---
+++ TheatreCMS3/Helpers/TextHelpers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheatreCMS3.Helpers
+{
+    
+        public static class TextHelpers
+        {
+            public static string TextHelp(string value, int number)
+            {
+
+
+            if (number >= value.Length)
+            {
+                return value;
+            }
+            else
+            {
+
+                return value.Substring(0, number) + "...";
+            }
+            }
+        }
+
+
+
+    
+}
