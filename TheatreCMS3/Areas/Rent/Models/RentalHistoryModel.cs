e15c90e9209cbacf2e42a59c0f622a7658d8abba
brianl628 <118641307+brianl628@users.noreply.github.com>
2023-04-07 18:48:27 -0500

    rentalhistory

---
+++ TheatreCMS3/Areas/Rent/Models/RentalHistoryModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TheatreCMS3.Models;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace TheatreCMS3.Areas.Rent.Models
+{
+    public class RentalHistoryModel
+    {
+        public int RentalHistoryId { get; set; }
+        public bool RentalDamaged { get; set; }
+        public string DamagesIncurred { get; set; }
+        public string Rental { get; set; }
+
+    }
+}
