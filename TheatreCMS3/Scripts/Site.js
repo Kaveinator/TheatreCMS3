5b454836f6d2f8f578b4eb45477a2f8bd1184959
brianl628 <118641307+brianl628@users.noreply.github.com>
2023-04-12 01:10:28 -0500

    Update Styling

--- TheatreCMS3/Scripts/Site.js
+++ TheatreCMS3/Scripts/Site.js
@@ -3,19 +3,4 @@ var Persons = $('#PersonList p').length;
 $('#NumPersons').text(Persons);
 });
 
-$(document).ready(function () {
-    var label = $('#damagesLabel');
-    var checkbox = $('#RentalDamaged');
-
-    label.text('Notes');
-    label.css('display', 'inline-block');
-
-    checkbox.click(function () {
-        if (this.checked) {
-            label.text('Damages' + ' Incurred');
-        } else {
-            label.text('Notes');
-        }
-    });
-});
 
