13a091b40a1ffc43e72f70b65617907eb01f5908
markedin <119455170+markedin@users.noreply.github.com>
2023-04-20 11:28:12 -0700

    Styling the comments in the blog area as well as creating a method to calculate and display how long it has been since a comment has been posted.

--- TheatreCMS3/Areas/Blog/Models/Comment.cs
+++ TheatreCMS3/Areas/Blog/Models/Comment.cs
@@ -28,6 +28,35 @@ namespace TheatreCMS3.Areas.Blog.Models
             double ratio = ((double)likes) / ((double)likes + (double)dislikes) * 100;
             return ratio;
         }
+        /// <summary>
+        /// Takes a DateTime and tells you how many days, hours, minutes, and 
+        /// seconds have passed since the input parameter
+        /// </summary>
+        /// <param name="commentDate"></param>
+        /// <returns>Returns a formatted string telling you how long it has been since the inputted date time. </returns>
+        public static string CommentTimePassed(DateTime commentDate)
+        {
+            string returnString = "";
+            List<string> returnStringList = new List<string>();
+            returnStringList.Add((DateTime.Now - commentDate).Days.ToString() + " Days,");
+            returnStringList.Add((DateTime.Now - commentDate).Hours.ToString() + " Hours,");
+            returnStringList.Add((DateTime.Now - commentDate).Minutes.ToString() + " Minutes,");
+            returnStringList.Add((DateTime.Now - commentDate).Seconds.ToString() + " Seconds ago");
+            for (int i = 0; i < returnStringList.Count; i++)
+            {
+                if (returnStringList[0].Contains("0 "))
+                {
+                    returnStringList.RemoveAt(0);
+                }
+            }
+
+            for (int i = 0; i < returnStringList.Count; i++)
+            {
+                returnString += returnStringList[i].ToString() + " ";
+            }
+
+            return returnString;
+        }
     }
 
 
