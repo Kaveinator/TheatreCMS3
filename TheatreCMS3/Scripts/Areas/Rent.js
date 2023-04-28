dc055b4704d21d27fc100a5b91cdc2581aaec765
Michael1388 <119345695+Michael1388@users.noreply.github.com>
2023-04-28 13:58:17 -0500

    Rental History CRUD Part #1: Create & Edit Page

--- TheatreCMS3/Scripts/Areas/Rent.js
+++ TheatreCMS3/Scripts/Areas/Rent.js
@@ -1,18 +1,8 @@
-﻿////$(document).ready(function () {
+﻿$('#RentalDamaged').click(function () {
 
-////    $(('#RentalDamaged')).click(function () {
-
-////        alert('You clicked!');
-
-////    });
-
-////});
-
-function Damaged() {
     if ($('#RentalDamaged').is(':checked')) {
         $('.rental_history-create--damagesincurred').html('Damages Incurred');
     } else {
         $('.rental_history-create--damagesincurred').html('Notes');
     }
-}
-
+});
