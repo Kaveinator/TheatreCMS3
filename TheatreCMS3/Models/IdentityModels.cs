a52173cb3e2694ddf81b4c66e9fb8a39da517606
elliemonroy <122836246+elliemonroy@users.noreply.github.com>
2023-05-10 09:19:40 -0700

    Create BlogAuthor Model & CRUD pages.

--- TheatreCMS3/Models/IdentityModels.cs
+++ TheatreCMS3/Models/IdentityModels.cs
@@ -48,6 +48,9 @@ namespace TheatreCMS3.Models
         public System.Data.Entity.DbSet<TheatreCMS3.Areas.Prod.Models.CastMember> CastMembers { get; set; }
 
         public System.Data.Entity.DbSet<TheatreCMS3.Areas.Prod.Models.CalendarEvent> CalendarEvents { get; set; }
+
+        public System.Data.Entity.DbSet<TheatreCMS3.Areas.Blog.Models.BlogAuthor> BlogAuthors { get; set; }
+
     }
 
 
