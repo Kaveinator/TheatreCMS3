a4523db136e60e556c0f9a24e9728c1ed73071a4
rowanspake <109189967+rowanspake@users.noreply.github.com>
2023-05-21 22:03:49 -0700

    Creates entity model for CalendarEvent and CRUD pages

--- TheatreCMS3/Models/IdentityModels.cs
+++ TheatreCMS3/Models/IdentityModels.cs
@@ -49,6 +49,8 @@ namespace TheatreCMS3.Models
 
         public System.Data.Entity.DbSet<TheatreCMS3.Areas.Blog.Models.BlogAuthor> BlogAuthors { get; set; }
 
+        public System.Data.Entity.DbSet<TheatreCMS3.Areas.Prod.Models.CalendarEvent> CalendarEvent { get; set; }
+
     }
 
 
