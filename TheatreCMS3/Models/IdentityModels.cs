9368798c94dec13d5158578eaab60c9578661648
Gabe Larson <gabriel.g.larson@gmail.com>
2023-06-12 10:03:05 -0700

    reset prod and rent areas with reserved stories

--- TheatreCMS3/Models/IdentityModels.cs
+++ TheatreCMS3/Models/IdentityModels.cs
@@ -43,15 +43,6 @@ namespace TheatreCMS3.Models
 
 
         /* ▼ Put DbSet's for your models below ▼ */
-
-        public System.Data.Entity.DbSet<TheatreCMS3.Areas.Rent.Models.RentalHistory> RentalHistories { get; set; }
-
-        public System.Data.Entity.DbSet<TheatreCMS3.Areas.Rent.Models.Rental> Rentals { get; set; }
-
-        public System.Data.Entity.DbSet<TheatreCMS3.Areas.Prod.Models.Production> Productions { get; set; }
-        public System.Data.Entity.DbSet<TheatreCMS3.Areas.Prod.Models.Castmember> Castmembers { get; set; }
-
-        public System.Data.Entity.DbSet<TheatreCMS3.Areas.Prod.Models.CalenderEvent> CalenderEvents { get; set; }
         public System.Data.Entity.DbSet<TheatreCMS3.Areas.Blog.Models.Comment> Comments { get; set; }
     }
 
