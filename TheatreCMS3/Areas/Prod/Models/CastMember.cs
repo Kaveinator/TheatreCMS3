e804f11a5406b1d706c7841806291dd79b4c71f5
tanyagalas <tanyashevts@gmail.com>
2023-06-16 10:47:41 -0700

    commit

--- TheatreCMS3/Areas/Prod/Models/CastMember.cs
+++ TheatreCMS3/Areas/Prod/Models/CastMember.cs
@@ -7,5 +7,27 @@ namespace TheatreCMS3.Areas.Prod.Models
 {
     public class CastMember
     {
+        public int CastMemberId { get; set; }
+        public string Name { get; set; }
+        public int? YearJoined { get; set; }
+        public Position MainRole { get; set; }
+        public string Bio { get; set; }
+        //public Byte[] Photo { get; set; }
+        public bool CurrentMember { get; set; }
+        public string Character { get; set; }
+        public int? CastYearLeft { get; set; }
+        public int? DebutYear { get; set; }
+        public enum Position
+        {
+            Actor,
+            Director,
+            Technicican,
+            StageManager,
+            Other
+        }
+
     }
+
+
 }
+  
