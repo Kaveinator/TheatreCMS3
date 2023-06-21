83473797683082e6953af4109a25338dc5c222c5
AngelV02 <125764846+AngelV02@users.noreply.github.com>
2023-06-21 15:12:08 -0700

    Added a Entity Model for Rental History

---
+++ TheatreCMS3/Areas/Rent/Models/RentalHistory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TheatreCMS3.Areas.Rent.Models
+{
+    public class RentalHistory
+    {
+        [Key]
+        public int RentalHistoryId { get; set; }
+        public bool RentalDamaged { get; set; }
+        public string DamagesIncurred { get; set; }
+        public string Rental { get; set; }
+
+
+    }
+
+    public class RentalContext : DbContext
+    {
+        public RentalContext() : base("RentalContext")
+        {
+        }
+
+        public DbSet<RentalHistory> RentalHistories { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<RentalHistory>().ToTable("RentalHistory");
+        }
+    }
+}
