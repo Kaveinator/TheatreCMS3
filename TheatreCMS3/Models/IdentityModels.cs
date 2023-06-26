ab94b39113c6d3d542fd5ebb00d5366550b866a6
Gabe Larson <gabriel.g.larson@gmail.com>
2023-06-26 10:45:04 -0700

    Reset Prod Area, reserved stories, and migration mishap

--- TheatreCMS3/Models/IdentityModels.cs
+++ TheatreCMS3/Models/IdentityModels.cs
@@ -43,9 +43,7 @@ namespace TheatreCMS3.Models
 
 
         /* ▼ Put DbSet's for your models below ▼ */
-        public System.Data.Entity.DbSet<TheatreCMS3.Areas.Prod.Models.CastMember> CastMembers { get; set; }
-
-        public System.Data.Entity.DbSet<TheatreCMS3.Areas.Prod.Models.CalendarEvents> CalendarEvents { get; set; }
+     
     }
 
 
