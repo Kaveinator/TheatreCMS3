04b4896998be1e789aa9ae5fef5e25b57596873d
Danielhmiller <107880080+Danielhmiller@users.noreply.github.com>
2023-06-29 15:09:54 -0500

    BlogAuthor Model Created with CRUD options.

---
+++ TheatreCMS3/Areas/Blog/Models/BlogAuthor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TheatreCMS3.Models;
+
+namespace TheatreCMS3.Areas.Blog.Models
+{
+    public class BlogAuthor
+    {
+        public int BlogAuthorId { get; set; }
+        public string Name { get; set; }
+        public string Bio { get; set; }
+        public DateTime JoinedDate { get; set; }
+        public DateTime LeftDate { get; set; }
+    }
+}
--- TheatreCMS3/Models/IdentityModels.cs
+++ TheatreCMS3/Models/IdentityModels.cs
@@ -40,10 +40,12 @@ namespace TheatreCMS3.Models
 
         }
 
+        public System.Data.Entity.DbSet<TheatreCMS3.Areas.Blog.Models.BlogAuthor> BlogAuthors { get; set; }
+
 
 
         /* ▼ Put DbSet's for your models below ▼ */
-     
+
     }
 
 
