b33b0f54742e6583b8b9c0947f34551918dd0887
jordlewis <ohgoodjord@gmail.com>
2023-07-14 18:48:03 -0500

    Adjustments

--- TheatreCMS3/Scripts/Areas/Rent.js
+++ TheatreCMS3/Scripts/Areas/Rent.js
@@ -1,11 +1,23 @@
-﻿$('#check-box').change(function () {
-    console.log('why are you not working??')
-    setLabel();
+﻿$(document).ready(function () {
+    $('#check-box').change(function () {
+        if ($(this).is(':checked')) {
+            $('#label-id').text('Damages Incurred');
+        } else {
+            $('#label-id').text('Notes');
+        }
+    });
 });
 
-function setLabel() {
-    var damageTrue = $('#check-box').is(':checked');
-    var damageNotes = damageTrue ? 'Damages Incurred' : 'Notes';
-    var damagesIncurredLabel = $('#damages-incurred-label'); 
-    damagesIncurredLabel.text(damageNotes);
-}
+
+$('.dropdown').find('.dropdown-toggle').hide();
+
+$(document).ready(function () {
+    $('td').hover(
+        function () {
+            $(this).find('.dropdown-toggle').show();
+        },
+        function () {
+            $(this).find('.dropdown-toggle').hide();
+        }
+    );
+});
