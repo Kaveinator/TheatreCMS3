f749d40ae1fb3ba35418a03a4181d09bcc49c137
Joel Johnson <82834522+joelmindset@users.noreply.github.com>
2023-07-17 10:18:09 -0600

    Reset Areas

--- TheatreCMS3/Models/IdentityModels.cs
+++ TheatreCMS3/Models/IdentityModels.cs
@@ -40,11 +40,8 @@ namespace TheatreCMS3.Models
 
         }
         /* ▼ Put DbSet's for your models below ▼ */
-        public System.Data.Entity.DbSet<TheatreCMS3.Areas.Blog.Models.BlogAuthor> BlogAuthors { get; set; }
 
-        public System.Data.Entity.DbSet<TheatreCMS3.Areas.Blog.Models.BlogPhoto> BlogPhotos { get; set; }
 
-        public System.Data.Entity.DbSet<TheatreCMS3.Areas.Rent.Models.RentalHistory> RentalHistories { get; set; }
     }
 
 
