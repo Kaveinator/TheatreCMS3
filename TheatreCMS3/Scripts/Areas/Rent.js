c1ddd12fa1dd6e45c6fee27fdd6ac151e049aa83
WatchRinseRepeat <s.a.eyring@gmail.com>
2023-07-19 12:08:41 -0600

    Complete CRUD part 4 story

--- TheatreCMS3/Scripts/Areas/Rent.js
+++ TheatreCMS3/Scripts/Areas/Rent.js
@@ -1,4 +1,5 @@
-﻿$(document).ready(function () {
+﻿$   //Function for changing the lable between notes and damage incurred
+    (document).ready(function () {
     
     if ($('.check-box').is(':checked')) {
         $('.label-id').text('Damages Incurred');
@@ -16,6 +17,8 @@
     });
 });
 
+
+//Funcitons for showing and hiding the dropdown button
 $('.dropdown').find('.dropdown-btn').hide();                      //hide the dropdown button on load
 
 $(document).ready(function () {
@@ -29,13 +32,14 @@ $(document).ready(function () {
     );
 });
 
+//Function for sorting the table
 $(document).ready(function () {
     $('#DropDownList').change(                                                   //On the change of the dropdown
         function () {
             if ($('#DropDownList option:selected').val() == "A-Z") {
                 //console.log("A-Z");
                 var $table = $('.rental-items');     // In the rental-items table
-                var rows = $table.find('tr').get();            //get all of the tr elements
+                var rows = $table.find('li').get();            //get all of the tr elements
 
                 rows.sort(function (a, b) {
                     var KeyA = $(a).attr('name');              //sort the tr elements based on the name attribute
@@ -46,12 +50,12 @@ $(document).ready(function () {
                 });
 
                 $.each(rows, function (index, row) {          //rewrite the table-items in the sorted order
-                    $table.children('tbody').append(row);
+                    $table.children('.tbody').append(row);
                 });
             } else if ($('#DropDownList option:selected').val() == "Z-A") {
                 //console.log("Z-A");
                 var $table = $('.rental-items');
-                var rows = $table.find('tr').get();
+                var rows = $table.find('li').get();
 
                 rows.sort(function (a, b) {
                     var KeyA = $(a).attr('name');                //sort the tr elements based on the name attribute
@@ -62,44 +66,41 @@ $(document).ready(function () {
                 });
 
                 $.each(rows, function (index, row) {
-                    $table.children('tbody').append(row);
+                    $table.children('.tbody').append(row);
                 });
             } else if ($('#DropDownList option:selected').val() == "Damaged") {
                 //console.log("Damaged");
                 var $table = $('.rental-items');
-                var rows = $table.find('tr').get();
+                var rows = $table.find('li').get();
 
                 rows.sort(function (a, b) {
-                    var KeyA = $(a).attr('damaged');         //sort the tr elements based on the damaged attribute
+                    var KeyA = $(a).attr('damaged');                //sort the tr elements based on the name attribute
                     var KeyB = $(b).attr('damaged');
-                    if (KeyA > KeyB) return 1;
-                    if (KeyA < KeyB) return -1;
-                    return 0;
+                    return (KeyA === KeyB) ? 0 : KeyA ? -1 : 1;         //sort the tr elements based on the damaged attribute
+                                                               //checks if a is exactly equal to b
                 });
 
                 $.each(rows, function (index, row) {
-                    $table.children('tbody').append(row);
+                    $table.children('.tbody').append(row);
                 });
             } else if ($('#DropDownList option:selected').val() == "Undamaged") {
                 //console.log("Undamaged");
                 var $table = $('.rental-items');
-                var rows = $table.find('tr').get();
+                var rows = $table.find('li').get();
 
                 rows.sort(function (a, b) {
-                    var KeyA = $(a).attr('damaged');
-                    var KeyB = $(b).attr('damaged');        //sort the tr elements based on the damaged attribute
-                    if (KeyA < KeyB) return 1;
-                    if (KeyA > KeyB) return -1;
-                    return 0;
+                    var KeyA = $(a).attr('damaged');                //sort the tr elements based on the name attribute
+                    var KeyB = $(b).attr('damaged');
+                    return (KeyA === KeyB) ? 0 : KeyA ? 1 : -1;         //sort the tr elements based on the damaged attribute
                 });
 
                 $.each(rows, function (index, row) {
-                    $table.children('tbody').append(row);
+                    $table.children('.tbody').append(row);
                 });
             } else {
                 //console.log("First to Last");
                 var $table = $('.rental-items');
-                var rows = $table.find('tr').get();
+                var rows = $table.find('li').get();
 
                 rows.sort(function (a, b) {
                     var KeyA = $(a).attr('sortId');                  //sort the tr elements based on the sortId attribute
@@ -110,7 +111,7 @@ $(document).ready(function () {
                 });
 
                 $.each(rows, function (index, row) {
-                    $table.children('tbody').append(row);
+                    $table.children('.tbody').append(row);
                 });
             }
             
