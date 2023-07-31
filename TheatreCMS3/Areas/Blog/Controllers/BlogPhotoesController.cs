bcb0606dc972e64423cee57d1b3c3250495c5eae
ZacButt <zacbutt8787@gmail.com>
2023-07-31 14:30:39 -0700

    BlogPhotoStorageProject
    Added to my code so the user can upload images.

--- TheatreCMS3/Areas/Blog/Controllers/BlogPhotoesController.cs
+++ TheatreCMS3/Areas/Blog/Controllers/BlogPhotoesController.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.IO; // Added for MemoryStream
 using System.Linq;
 using System.Net;
+using System.Threading.Tasks; // Added for async/await
 using System.Web;
 using System.Web.Mvc;
 using TheatreCMS3.Areas.Blog.Models;
@@ -47,10 +49,16 @@ namespace TheatreCMS3.Areas.Blog.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "BlogPhotoId,Title,Photo")] BlogPhoto blogPhoto)
+        public async Task<ActionResult> Create([Bind(Include = "BlogPhotoId,Title")] BlogPhoto blogPhoto, HttpPostedFileBase imageFile) 
         {
-            if (ModelState.IsValid)
+            if (ModelState.IsValid && imageFile != null && imageFile.ContentLength > 0)
             {
+                using (var memoryStream = new MemoryStream())
+                {
+                    imageFile.InputStream.CopyTo(memoryStream);
+                    blogPhoto.Photo = memoryStream.ToArray();
+                }
+
                 db.BlogPhotoes.Add(blogPhoto);
                 db.SaveChanges();
                 return RedirectToAction("Index");
@@ -79,14 +87,25 @@ namespace TheatreCMS3.Areas.Blog.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "BlogPhotoId,Title,Photo")] BlogPhoto blogPhoto)
+        public async Task<ActionResult> Edit([Bind(Include = "BlogPhotoId,Title,Photo")] BlogPhoto blogPhoto) 
         {
             if (ModelState.IsValid)
             {
+                if (blogPhoto.Photo != null && blogPhoto.Photo.Length > 0)
+                {
+                    using (var memoryStream = new MemoryStream(blogPhoto.Photo))
+                    {
+                        var photoBytes = new byte[memoryStream.Length];
+                        await memoryStream.ReadAsync(photoBytes, 0, photoBytes.Length);
+                        blogPhoto.Photo = photoBytes;
+                    }
+                }
+
                 db.Entry(blogPhoto).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
+
             return View(blogPhoto);
         }
 
@@ -123,6 +142,18 @@ namespace TheatreCMS3.Areas.Blog.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
+        }
+
+        // Method to retrieve the image from the database based on the Id
+        [HttpGet]
+        public ActionResult GetImage(int id)
+        {
+            var blogPhoto = db.BlogPhotoes.Find(id);
+            if (blogPhoto != null && blogPhoto.Photo != null)
+            {
+                return new FileContentResult(blogPhoto.Photo, "image/jpeg"); // Modify the content type according to the image type you are using (e.g., image/jpeg for JPEG images)
+            }
+            return null; 
         }
     }
 }
