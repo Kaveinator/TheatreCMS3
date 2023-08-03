a37050e139e363b33f4145e603206f6da042170c
Natty Sanchez <84761201+natty886@users.noreply.github.com>
2023-08-03 15:14:32 -0700

    fixed label "Damages Incurred" to be on one line on Edit page.

--- TheatreCMS3/Scripts/Areas/Rent.js
+++ TheatreCMS3/Scripts/Areas/Rent.js
@@ -1,11 +1,18 @@
-﻿console.log("Hello World!");
+﻿var checkbox = $('.checkbox input[type="checkbox"]');
+var isChecked = checkbox.prop("checked");
+var label = $('.renthistory-damage');
+    if (isChecked) {
+        label.text('Damages Incurred');
+    } else {
+        label.text('Notes');
+    }
+
 $(document).ready(function () {
-    var checkbox = $('.checkbox');
+    var checkbox = $('.checkbox input[type="checkbox"]');
     var label = $('.renthistory-damage');
 
     checkbox.on('change', function () {
         var checked = this.checked;
-
         if (checked) {
             label.text('Damages Incurred');
         } else {
