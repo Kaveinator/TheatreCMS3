554ada90a587dd0888c869ca5dbfafc93f6c84b8
Natty Sanchez <84761201+natty886@users.noreply.github.com>
2023-08-03 14:35:46 -0700

    styled Create page and added jquery to checkbox

--- TheatreCMS3/Scripts/Areas/Rent.js
+++ TheatreCMS3/Scripts/Areas/Rent.js
@@ -1,1 +1,15 @@
-﻿
+﻿console.log("Hello World!");
+    $(document).ready(function () {
+            var checkbox = $('.checkbox');
+    var label = $('.renthistory-damage');
+
+    checkbox.on('change', function () {
+                var checked = this.checked;
+
+    if (checked) {
+        label.text('Damages Incurred');
+                } else {
+        label.text('Notes');
+                }
+            });
+        });
