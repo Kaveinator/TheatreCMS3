1fcab760f1c7b4ab73f5017d0358971cff8201a7
BretMorrisGitHub <bret.morris0905@gmail.com>
2023-08-10 13:53:47 -0700

    Create RequestManager commit

---
+++ TheatreCMS3/Areas/Rent/Models/RequestManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TheatreCMS3.Models;
+
+namespace TheatreCMS3.Areas.Rent.Models
+{
+    public class RequestManager : ApplicationUser
+    {
+        public int RejectedRequests { get; set; }
+        public int AcceptedRequests { get; set; }
+    }
+}
