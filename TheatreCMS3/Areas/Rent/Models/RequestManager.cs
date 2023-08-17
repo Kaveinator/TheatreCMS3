891775043ce71485c295f1200ab74fb5554980fc
BretMorrisGitHub <bret.morris0905@gmail.com>
2023-08-17 10:38:31 -0700

    Seed Request Manager Commit

--- TheatreCMS3/Areas/Rent/Models/RequestManager.cs
+++ TheatreCMS3/Areas/Rent/Models/RequestManager.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using TheatreCMS3.Models;
 
 namespace TheatreCMS3.Areas.Rent.Models
@@ -11,9 +13,31 @@ namespace TheatreCMS3.Areas.Rent.Models
         public int RejectedRequests { get; set; }
         public int AcceptedRequests { get; set; }
 
-        public static SeedRequestManager()
+        public static void SeedRequestManager(ApplicationDbContext context)
         {
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var roleUserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
 
+            if (!roleManager.RoleExists("RentalManager"))
+            {
+                var requestRole = new IdentityRole();
+                requestRole.Name = "RentalManager";
+                roleManager.Create(requestRole);
+
+                var rentalManager = new RequestManager
+                {
+                    UserName = "RentalManager",
+                    Email = "RentalManager@gmail.com",
+                };
+
+                string password = "Password123!";
+                var requestManagerResult = roleUserManager.Create(rentalManager, password);
+
+                if (requestManagerResult.Succeeded)
+                {
+                    roleUserManager.AddToRole(rentalManager.Id, "RentalManager");
+                }
+            }
         }
     }
 }
