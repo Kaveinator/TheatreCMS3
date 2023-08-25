7935c230f7d7107c9696063df0c582ea0b09a9c1
ksalvo15 <121691678+ksalvo15@users.noreply.github.com>
2023-08-25 15:22:16 -0700

    most styling is done, working on RentalDamaged checkboxes

--- TheatreCMS3/Scripts/Areas/Rent.js
+++ TheatreCMS3/Scripts/Areas/Rent.js
@@ -1,13 +1,15 @@
 ﻿$(document).ready(function () {
     $("#myCheck input[type='checkbox']").click(function () {
         var label = $("label[for='DamagesIncurred']");
+        var checkbox = $(this);
 
-        if (this.checked) {
+        if (checkbox.prop("checked")) {
             label.text("Damages Incurred");
+           /* $(".RH-index-damaged").html('<span class="bi bi-x-circle-fill red-color"></span>');*/
         } else {
             label.text("Notes");
+            /*$(".RH-index-damaged").html('<span class="bi bi-check-circle-fill green-color"></span>');*/
         }
     });
 });
-
 
