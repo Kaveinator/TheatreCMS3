8e2961b0ed97a4fd93b9593ec2550c6f88cd0876
ksalvo15 <121691678+ksalvo15@users.noreply.github.com>
2023-08-29 15:16:15 -0700

    edits to the sort button

--- TheatreCMS3/Scripts/Areas/Rent.js
+++ TheatreCMS3/Scripts/Areas/Rent.js
@@ -12,3 +12,47 @@
     });
 });
 
+function sortList() {
+    var table, rows, switching, i, x, y, shouldSwitch;
+    table = document.getElementsByClassName("RH-index-rentalname");
+    switching = true;
+
+    while (switching) {
+        switching = false;
+        rows = table.rows;
+
+        for (i = 1; i < (rows.length - 1); i++) {
+            shouldSwitch = false;
+
+            x = rows[i].getElementsByClassName("RH-index-rentalname")[0];
+            y = rows[i + 1].getElementsByClassName("RH-index-rentalname")[0];
+            if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                shouldSwitch = true;
+                break;
+            }
+        }
+        if (shouldSwitch) {
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        }
+    }
+    
+}
+
+function myFunction() {
+    document.getElementByClass("RH-index-sortdrop").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.RH-index-sortdrop')) {
+        var dropdowns = document.getElementsByClassName("RH-index-sortcontent");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
