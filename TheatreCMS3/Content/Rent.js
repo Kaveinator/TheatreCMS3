9f40da38c32b741ff9b0e073bd61432eecdc4b20
Jake Rose <72412423+omniboss@users.noreply.github.com>
2023-09-08 16:35:28 -0700

    draft 3

--- TheatreCMS3/Content/Rent.js
+++ TheatreCMS3/Content/Rent.js
@@ -1,5 +1,15 @@
-﻿function changeFunction() {
-    var changes = "Damages Incurred";
-    document.getElementById("change").innerHTML = changes;
-}
+﻿
 
+function changeFunction() {
+    var changes = "Damages Incurred";
+    var changed = "Notes";
+    const checkBox = document.querySelector("#check");
+
+        if (checkBox.checked) {
+            document.getElementById("change").innerHTML = changes;
+        }
+        else {
+            document.getElementById("change").innerHTML = changed;
+        }
+    
+}
