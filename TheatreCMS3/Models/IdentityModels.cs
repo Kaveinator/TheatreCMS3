14e6ea7bfd5abc75b1f00c0c51dd8e838f592034
Joel Johnson <82834522+joelmindset@users.noreply.github.com>
2023-09-11 11:49:01 -0600

    Reset Blog

--- TheatreCMS3/Models/IdentityModels.cs
+++ TheatreCMS3/Models/IdentityModels.cs
@@ -43,10 +43,6 @@ namespace TheatreCMS3.Models
  
         /* ▼ Put DbSet's for your models below ▼ */
 
-        public System.Data.Entity.DbSet<TheatreCMS3.Areas.Blog.Models.BlogPhoto> BlogPhotoes { get; set; }
-
-        public System.Data.Entity.DbSet<TheatreCMS3.Areas.Blog.Models.BlogAuthor> BlogAuthors { get; set; }
-
         public System.Data.Entity.DbSet<TheatreCMS3.Areas.Rent.Models.RentalHistory> RentalHistories { get; set; }
     }
 
