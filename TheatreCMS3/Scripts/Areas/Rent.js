edf99f8701d98eb9a7b7c3e05fdce6a09d99f61e
Joel Johnson <82834522+joelmindset@users.noreply.github.com>
2023-09-13 13:27:08 -0600

    JS fix

--- TheatreCMS3/Scripts/Areas/Rent.js
+++ TheatreCMS3/Scripts/Areas/Rent.js
@@ -1,1 +1,11 @@
-﻿
+﻿$(document).ready(function () {
+    function toggleLabel() {
+        var isChecked = $('#RentalDamaged').is(':checked');
+        var label = isChecked ? 'Damages Incurred' : 'Notes';
+        $('#damageLabel').text(label);
+    }
+    toggleLabel();
+    $('#RentalDamaged').change(function () {
+        toggleLabel();
+    });
+});
