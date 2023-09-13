1bd748a5c41748eee01dddb8a5cb48db3786aa42
Jake Rose <72412423+omniboss@users.noreply.github.com>
2023-09-12 20:41:52 -0700

    1st draft

--- TheatreCMS3/Content/Rent.js
+++ TheatreCMS3/Content/Rent.js
@@ -9,3 +9,32 @@
         toggleLabel();
     });
 });
+
+$(document).ready(function () {
+    function toggleGraphic() {
+        var Checked = $('#RentalDamaged').is(':checked');
+        var Graphic = Checked ? 'x' : 'o' ;
+        $('#damageGraphic').text(label);
+    }
+    toggleLabel();
+    $('#damagedGraphic').change(function () {
+        toggleLabel();
+    });
+});
+
+$(".check").each(function () {
+    $(this).hide();
+
+    var $image = $(<i class="fa-solid fa-circle-xmark" style="color: #ff0000;"></i>).insertAfter(this);
+
+    $image.click(function () {
+        var $check-box = $(this.prev(".check");
+        $check-box.prop('checked', !$checkbox.prop('checked'));
+
+        if ($check-box.prop("checked")) {
+            $image.attr(<i class="fa-solid fa-square-check" style="color: #00ff40;"></i>);
+        } else {
+            $image.attr(<i class="fa-solid fa-circle-xmark" style="color: #ff0000;"></i>);
+        }
+    });
+});
