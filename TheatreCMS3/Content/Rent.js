a4141797df328b432c310f087bf037d334021b88
Jake Rose <72412423+omniboss@users.noreply.github.com>
2023-09-15 12:23:05 -0700

    error2

--- TheatreCMS3/Content/Rent.js
+++ TheatreCMS3/Content/Rent.js
@@ -1,4 +1,14 @@
-﻿$(document).ready(function () {
+﻿////const contain = document.getElementById('table');
+////const hiddenDiv = document.getElementById('dropdown');
+
+////contain.addEventListener('mouseover', function handleMouseOver() {
+////    hiddenDiv.style.display = 'block';
+////});
+$(document).ready(function () {
+    $(".dropdown").hide(onload)
+});
+
+$(document).ready(function () {
     function toggleLabel() {
         var isChecked = $('#RentalDamaged').is(':checked');
         var label = isChecked ? 'Damages Incurred' : 'Notes';
