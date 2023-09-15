d33262e0f9c8c68863951eca6554cf63a396d531
Joel Johnson <82834522+joelmindset@users.noreply.github.com>
2023-09-15 13:56:57 -0600

    js fix

--- TheatreCMS3/Scripts/Areas/Rent.js
+++ TheatreCMS3/Scripts/Areas/Rent.js
@@ -1,1 +1,26 @@
-﻿
+﻿$(".dropdown").hide(onload)
+
+$(document).ready(function () {
+    $('.rentContainer').hover(
+        function () {
+            $(this).find('.dropdown').show();
+        },
+        function () {
+            $(this).find('.dropdown').hide();
+        }
+    );
+});
+
+
+
+$(document).ready(function () {
+    function toggleLabel() {
+        var isChecked = $('#RentalDamaged').is(':checked');
+        var label = isChecked ? 'Damages Incurred' : 'Notes';
+        $('#damageLabel').text(label);
+    }
+    toggleLabel();
+    $('#RentalDamaged').change(function () {
+        toggleLabel();
+    });
+});
