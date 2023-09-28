e2cfc012bcb8751a041991bc097172b6b51c60d6
jbikeler <jbikeler@gmail.com>
2023-09-28 12:32:22 -0700

    CRUDp2 Styling Details Page

--- TheatreCMS3/Scripts/Areas/Blog.js
+++ TheatreCMS3/Scripts/Areas/Blog.js
@@ -1,9 +1,10 @@
 function toggleContent(contentID) {
     //hide all availabe content sections
-    for (x in getElementsByClassName("content")) {
+    for (x in document.getElementsByClassName("content")) {
         x.style.display = 'none';
     };
     //get passed content seaction id and set display to visible
     var elem = document.getElementById(contentID);
-        elem.style.display = 'block';
+    elem.style.display = 'block';
+    print("ran toggle")
     }
