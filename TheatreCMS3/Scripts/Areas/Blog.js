361868f54192b945d88a53cd3deadd389f932be2
jbikeler <jbikeler@gmail.com>
2023-09-28 13:47:56 -0700

    Updated CRUDp2
     added picture and styled buttons

--- TheatreCMS3/Scripts/Areas/Blog.js
+++ TheatreCMS3/Scripts/Areas/Blog.js
@@ -1,10 +1,10 @@
 function toggleContent(contentID) {
     //hide all availabe content sections
-    for (x in document.getElementsByClassName("content")) {
-        x.style.display = 'none';
+    var hideElems = document.getElementsByClassName("content")
+    for (let x = 0; x < hideElems.length; x++) {
+        hideElems[x].style.display = 'none';
     };
     //get passed content seaction id and set display to visible
     var elem = document.getElementById(contentID);
     elem.style.display = 'block';
-    print("ran toggle")
     }
