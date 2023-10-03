a67e49d64dfde54e71b2be08c5e7ca7634f64906
Joel Johnson <82834522+joelmindset@users.noreply.github.com>
2023-10-03 10:50:02 -0600

    reset rental

--- TheatreCMS3/Models/IdentityModels.cs
+++ TheatreCMS3/Models/IdentityModels.cs
@@ -40,7 +40,7 @@ namespace TheatreCMS3.Models
 
         }
 
-        public System.Data.Entity.DbSet<TheatreCMS3.Areas.Rent.Models.RentalHistory> RentalHistories { get; set; }
+        
 
 
         /* ▼ Put DbSet's for your models below ▼ */
