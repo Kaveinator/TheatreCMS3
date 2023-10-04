4160b4c5df19c6a9f4841bbb1388d69e58c7d0cc
jckyagno <133424867+jckyagno@users.noreply.github.com>
2023-10-04 13:19:18 -1000

    Completed Edit Page Overlay and text switch functionality

--- TheatreCMS3/Scripts/Site.js
+++ TheatreCMS3/Scripts/Site.js
@@ -18,18 +18,16 @@ function switchVisible() {
 }
 
 function changeDamagedToNotes() {
-    var checkbox = document.querySelector("input[id=RentalDamaged]");
-    checkbox.addEventListener('change', function () {
-        if (this.checked) {
-            console.log("Checkbox is checked..");
-            document.getElementById("damages-incurred").style.display = 'flex';
-            document.getElementById("notes").style.display = 'none';
-        } else {
-            console.log("Checkbox is not checked..");
-            document.getElementById("damages-incurred").style.display = 'none';
-            document.getElementById("notes").style.display = 'flex';
-        }
-    });
+    var checkbox = document.getElementById("RentalDamaged")
+    if (checkbox.checked == true) {
+        console.log("Checkbox is checked.." + checkbox.checked);
+        document.getElementById("damages-incurred").style.display = 'flex';
+        document.getElementById("notes").style.display = 'none';
+    } else {
+        console.log("Checkbox is not checked..");
+        document.getElementById("damages-incurred").style.display = 'none';
+        document.getElementById("notes").style.display = 'flex';
+    }
 }
 
 
