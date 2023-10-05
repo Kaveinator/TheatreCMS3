b3ed07ba087c8bd0aa3627281491c2c493dd37aa
jckyagno <133424867+jckyagno@users.noreply.github.com>
2023-10-05 09:08:49 -1000

    Completed format of Most recent Rental Histories table

--- TheatreCMS3/Scripts/Areas/Rent.js
+++ TheatreCMS3/Scripts/Areas/Rent.js
@@ -1,4 +1,5 @@
 ﻿changeDamagedToNotes();
+damageSymbol();
 
 function changeDamagedToNotes() {
     var checkbox = document.getElementById("RentalDamaged")
@@ -6,9 +7,23 @@ function changeDamagedToNotes() {
         console.log("Checkbox is checked.." + checkbox.checked);
         document.getElementById("damages-incurred").style.display = 'flex';
         document.getElementById("notes").style.display = 'none';
-    } else {
+    }
+    else {
         console.log("Checkbox is not checked..");
         document.getElementById("damages-incurred").style.display = 'none';
         document.getElementById("notes").style.display = 'flex';
+    }
+}
+
+function damageSymbol() {
+    var checkbox = document.getElementsByClassName("check-box")
+    if (checkbox.checked == true) {
+        console.log("test1");
+        document.getElementById("checkmark").style.display = 'flex';
+        document.getElementById("xmark").style.display = 'none';
+    } else {
+        console.log("test2");
+        document.getElementById("checkmark").style.display = 'none';
+        document.getElementById("xmark").style.display = 'flex';
     }
 }
