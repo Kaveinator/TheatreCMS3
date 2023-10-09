31fb3d31197ef6feee868c5652bf637d35c14dcf
caduque0906 <caduque0906@gmail.com>
2023-10-09 18:54:37 -0400

    Added model

---
+++ TheatreCMS3/Areas/Prod/Models/ProductionPhoto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheatreCMS3.Areas.Prod.Models
+{
+    public class ProductionPhoto
+    {
+        [Key]
+        public int ProPhotoId { get; set; }
+        //public byte[] PhotoFile { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
