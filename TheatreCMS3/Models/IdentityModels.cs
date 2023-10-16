cc75ad8a4169e5f10b085f04b72b2145fb6cefc1
Jeff <jstopp10@gmail.com>
2023-10-16 14:16:53 -0700

    Reset Blog & Prod Areas

--- TheatreCMS3/Models/IdentityModels.cs
+++ TheatreCMS3/Models/IdentityModels.cs
@@ -45,15 +45,7 @@ namespace TheatreCMS3.Models
 
         /* ▼ Put DbSet's for your models below ▼ */
 
-        public System.Data.Entity.DbSet<TheatreCMS3.Areas.Blog.Models.BlogAuthor> BlogAuthors { get; set; }
-
-        public System.Data.Entity.DbSet<TheatreCMS3.Areas.Prod.Models.CalendarEvent> CalendarEvents { get; set; }
-
-        object placeHolderVariable;
         public System.Data.Entity.DbSet<TheatreCMS3.Areas.Rent.Models.RentalHistory> RentalHistories { get; set; }
 
-        public System.Data.Entity.DbSet<TheatreCMS3.Areas.Blog.Models.BlogPhoto> BlogPhotoes { get; set; }
-
-        public System.Data.Entity.DbSet<TheatreCMS3.Areas.Prod.Models.ProductionPhoto> ProductionPhotoes { get; set; }
     }
 }
