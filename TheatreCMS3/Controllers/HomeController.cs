d03ab8485ca60a164f46c44198acc3dbbe5d8832
Jeff <jstopp10@gmail.com>
2023-10-16 13:48:42 -0700

    Reset Contact Page Reserve Story

--- TheatreCMS3/Controllers/HomeController.cs
+++ TheatreCMS3/Controllers/HomeController.cs
@@ -44,11 +44,6 @@ namespace TheatreCMS3.Controllers
         {
             return View();
         }
-        
-        public ActionResult ContactUs()
-        {
-            return View();
-        }
 
     }
 }
