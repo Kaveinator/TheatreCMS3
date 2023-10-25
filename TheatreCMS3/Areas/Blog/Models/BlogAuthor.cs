2f518b56a2ddcce9f83bac39f49720b1cc6e5398
jrheat2005 <jrheat2005@gmail.com>
2023-10-25 10:12:30 -0700

    Fixed data saving on BlogAuthor edit age

--- TheatreCMS3/Areas/Blog/Models/BlogAuthor.cs
+++ TheatreCMS3/Areas/Blog/Models/BlogAuthor.cs
@@ -13,6 +13,6 @@ namespace TheatreCMS3.Areas.Blog.Models
         public string Name { get; set; }
         public string Bio { get; set; }
         public DateTime Joined { get; set; }
-        public DateTime Left { get; set; }
+        public DateTime? Left { get; set; }
     }
 }
