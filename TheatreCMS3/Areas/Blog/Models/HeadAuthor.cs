115e6a369489807cce46e12ec9c6f92b65175618
jrheat2005 <jrheat2005@gmail.com>
2023-11-01 10:12:27 -0700

    Created Role and seeding method

--- TheatreCMS3/Areas/Blog/Models/HeadAuthor.cs
+++ TheatreCMS3/Areas/Blog/Models/HeadAuthor.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using TheatreCMS3.Models;
 
 namespace TheatreCMS3.Areas.Blog.Models
@@ -11,5 +10,47 @@ namespace TheatreCMS3.Areas.Blog.Models
         public int ViewsPerMonth { get; set; }
         public int AuthorsHired { get; set; }
         public int AuthorsLetGo { get; set; }
+    }
+    public class Seedata
+    {
+
+        public static void HeadAuthorSeed(IServiceProvider serviceProvider)
+        {
+
+            Console.WriteLine("Seeding the HeadAuthor...");
+            var userManager = new UserManager<HeadAuthor>(new UserStore<HeadAuthor>(new ApplicationDbContext()));
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new ApplicationDbContext()));
+
+            // Ensure the "HeadAuthor" role exists
+            if (!roleManager.RoleExists("HeadAuthor"))
+            {
+                var role = new IdentityRole { Name = "HeadAuthor" };
+                var roleResult = roleManager.Create(role);
+                if (!roleResult.Succeeded)
+                {
+                    throw new Exception("Failed to create the 'HeadAuthor' role.");
+                    return;
+                }
+            }
+
+            // Create and seed a HeadAuthor user
+            var headAuthor = new HeadAuthor
+            {
+                UserName = "HeadAuthorUsername",
+                ViewsPerMonth = 1000,
+                AuthorsHired = 5,
+                AuthorsLetGo = 1,
+            };
+
+            var userResult = userManager.Create(headAuthor, "YourPassword");
+            if (userResult.Succeeded)
+            {
+                userManager.AddToRole(headAuthor.Id, "HeadAuthor");
+            }
+            else
+            {
+                throw new Exception("Failed to create the HeadAuthor user.");
+            }
+        }
     }
 }
