5188a7c55638ca39aa38c975a8e72013569a4d89
J0SH001 <gysljosh@gmail.com>
2023-11-15 11:42:08 -0600

    updated model

--- TheatreCMS3/Areas/Prod/Models/EventPlanner.cs
+++ TheatreCMS3/Areas/Prod/Models/EventPlanner.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using TheatreCMS3.Models;
 
 namespace TheatreCMS3.Areas.Prod.Models
 {
-    public class EventPlanner : TheatreCMS3.Models.ApplicationUser
+    public class EventPlanner : ApplicationUser
     {
         public DateTime PlannerStartDate { get; set; }
     }
