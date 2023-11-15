e282b46674cb45f20cf5587cf3322918850976e5
J0SH001 <gysljosh@gmail.com>
2023-11-15 11:28:12 -0600

    Added Event Planner

---
+++ TheatreCMS3/Areas/Prod/Models/EventPlanner.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheatreCMS3.Areas.Prod.Models
+{
+    public class EventPlanner : TheatreCMS3.Models.ApplicationUser
+    {
+        public DateTime PlannerStartDate { get; set; }
+    }
+}
