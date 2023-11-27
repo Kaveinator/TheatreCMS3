84f066fd51e02b7e7794839468049e6c9e74752e
Joel Johnson <82834522+joelmindset@users.noreply.github.com>
2023-11-27 10:21:23 -0700

    Reset

--- TheatreCMS3/Migrations/Configuration.cs
+++ TheatreCMS3/Migrations/Configuration.cs
@@ -8,7 +8,7 @@
     using Microsoft.AspNet.Identity;
     using Microsoft.AspNet.Identity.EntityFramework;
     using TheatreCMS3.Models;
-    using TheatreCMS3.Areas.Prod.Models;
+   
 
     internal sealed class Configuration : DbMigrationsConfiguration<TheatreCMS3.Models.ApplicationDbContext>
     {
@@ -21,7 +21,7 @@
 
         protected override void Seed(ApplicationDbContext context)
         {
-            EventPlanner.Seed(context);
+
         }
     }
 
--- TheatreCMS3/Models/IdentityModels.cs
+++ TheatreCMS3/Models/IdentityModels.cs
@@ -45,8 +45,6 @@ namespace TheatreCMS3.Models
 
         /* ▼ Put DbSet's for your models below ▼ */
 
-        public System.Data.Entity.DbSet<TheatreCMS3.Areas.Prod.Models.CalendarEvent> CalendarEvents { get; set; }
 
-        public System.Data.Entity.DbSet<TheatreCMS3.Areas.Blog.Models.BlogAuthor> BlogAuthors { get; set; }
     }
 }
