a78216ee5f06636d129863732f7226a072a5acfe
NReese650 <128111600+NReese650@users.noreply.github.com>
2023-12-20 11:54:17 -0800

    Adding EventPlanner model

---
+++ TheatreCMS3/Areas/Prod/Models/EventPlanner.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheatreCMS3.Models
+{
+    public class EventPlanner : ApplicationUser
+    {
+        public DateTime PlannerStartDate { get; set; }
+    }
+}
