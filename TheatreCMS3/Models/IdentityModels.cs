25c07864d1fc9192b1ef027cd549fbe4e79353f4
kxxy:kodez <135093578+krys-tsuda@users.noreply.github.com>
2023-12-29 10:30:47 -0800

    Revert "BlogAuthor CRUD"
    This reverts commit 548cf09dd16c1a74e289dbd39d077a952ea2558a.

--- TheatreCMS3/Models/IdentityModels.cs
+++ TheatreCMS3/Models/IdentityModels.cs
@@ -46,8 +46,6 @@ namespace TheatreCMS3.Models
 
         public System.Data.Entity.DbSet<TheatreCMS3.Areas.Prod.Models.ProductionPhoto> ProductionPhotoes { get; set; }
 
-        public System.Data.Entity.DbSet<TheatreCMS3.Areas.Blog.Models.BlogAuthor> BlogAuthors { get; set; }
-
 
 
 
