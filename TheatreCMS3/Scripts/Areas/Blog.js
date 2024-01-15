4236616592ee5cb52fbfcf34fbea5c9bd433dbbc
Tanner Vasquez <126614745+TVisi0n@users.noreply.github.com>
2024-01-15 12:01:45 -0800

    styled and added functionality to details and delete pages

--- TheatreCMS3/Scripts/Areas/Blog.js
+++ TheatreCMS3/Scripts/Areas/Blog.js
@@ -14,11 +14,31 @@ $(document).ready(function () {
     });
 });
 
-
 // funciton to hide/display navigation tabs on details/delete view
 function toggleView(view) {
     if (view === 'authorDetails') {
         $('#authorDetailsTab').tab('show');
     } else if (view === 'blogPosts') {
         $('#blogPostsTab').tab('show');
-}
+    }
+};
+
+//function toggle(id) {
+//    var e = document.getElementById(id);
+//    if (e.style.display == 'block')
+//        e.style.display = 'none';
+//    else
+//        e.style.display = 'block';
+//}
+
+//$(document).ready(function () {
+//    $(".nav-tabs a").click(function () {
+//        $(this).tab('show');
+//    });
+//});
+
+function activaTab(tab) {
+    $('.nav-tabs a[href="#' + tab + '"]').tab('show');
+};
+
+activaTab('authorDetailsTab');
