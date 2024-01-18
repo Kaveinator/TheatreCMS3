7fcb653f02fac1390d912447604b432f294ac6a0
Tanner Vasquez <126614745+TVisi0n@users.noreply.github.com>
2024-01-18 10:35:52 -0800

    Ajax

--- TheatreCMS3/Scripts/Areas/Blog.js
+++ TheatreCMS3/Scripts/Areas/Blog.js
@@ -23,22 +23,59 @@ function toggleView(view) {
     }
 };
 
-//function toggle(id) {
-//    var e = document.getElementById(id);
-//    if (e.style.display == 'block')
-//        e.style.display = 'none';
-//    else
-//        e.style.display = 'block';
+//function deleteAuthor() {
+//    //var getId = $.ajax({
+//    //    type: "GET",
+//    //    url: "/BlogAuthors/Delete/" + Id
+//    /*})*/
+//    if (confirm("Are you sure you want to delete this author?")) {
+//            console.log("Hello");
+//    } else {
+//        console.log("Hello1");
+//    }
 //}
 
-//$(document).ready(function () {
-//    $(".nav-tabs a").click(function () {
-//        $(this).tab('show');
-//    });
-//});
+//var modal = document.getElementById('simpleModal');
+//var modalBtn = document.getElementById('modalBtn');
+//var closeBtn = document.getElementsByClassName('closeBtn')[0];
 
-function activaTab(tab) {
-    $('.nav-tabs a[href="#' + tab + '"]').tab('show');
-};
 
-activaTab('authorDetailsTab');
+//modalBtn.addEventListener('click', openModal);
+
+//closeBtn.addEventListener('click', closeModal);
+
+//window.addEventListener('click', outsideClick);
+
+//function openModal() {
+//    modal.style.display = 'block';
+//}
+
+//function closeModal() {
+//    modal.style.display = 'none';
+//}
+
+//function outsideClick(e) {
+//    if (e.target == modal) {
+//        modal.style.display = 'none';
+//    }
+//}
+
+$(document).ready(function () {
+    $('.deleteAuthor').click(function (x) {
+        x.preventDefault();
+        var authorRequestId = $(this).data('indexDelete')
+        console.log(authorRequestId);
+        if (confirm("are you sure you want to delete the author?")) {
+            $.ajax({
+                url: '/blogauthors/delete/' + authorrequestid,
+                type: 'post',
+                success: function (result) {
+                    console.log(result)
+                },
+                error: function (err) {
+                    console.log(err);
+                }
+            });
+        }
+    });
+});
