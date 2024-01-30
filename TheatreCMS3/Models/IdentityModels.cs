eeaab1c03aa64bd5fa0dcf4a7ac379aab11f1e68
Jiaha0-Zhang <xihd2017@gmail.com>
2024-01-30 12:01:22 -0800

    Create entity model for CalendarEvent and CRUD pages

--- TheatreCMS3/Models/IdentityModels.cs
+++ TheatreCMS3/Models/IdentityModels.cs
@@ -40,6 +40,8 @@ namespace TheatreCMS3.Models
 
         }
 
+        public System.Data.Entity.DbSet<TheatreCMS3.Areas.Prod.Models.CalendarEvent> CalendarEvents { get; set; }
+
         /* ▼ Put DbSet's for your models below ▼ */
 
 
