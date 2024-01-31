a0937cd1ba8a6b1d1b81b1012b5ebb14cff8fd4d
joey11602 <joey35475@outlook.com>
2024-01-30 19:08:58 -0600

    save models

---
+++ TheatreCMS3/Areas/Blog/Models/BlogAuthor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheatreCMS3.Areas.Blog.Models
+{
+    public class BlogAuthor
+    {
+        //primary Key
+        public int BlogAuthorId { get; set; }
+        //properties
+        public string Name { get; set; }
+        public string Bio { get; set; }
+        public DateTime Joined { get; set; }
+        public DateTime? Left { get; set; } //nullable DateTime for when the author leaves
+    }
+}
---
+++ TheatreCMS3/Areas/Blog/Models/BlogDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TheatreCMS3.Areas.Blog.Models
+{
+    public class BlogDbContext : DbContext
+    {
+        public DbSet<BlogAuthor> BlogAuthors { get; set; }
+    }
+}
