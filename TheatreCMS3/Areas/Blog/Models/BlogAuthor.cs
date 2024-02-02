710c514cdd01f72525edbcc4a64ddc25ccb7cf4f
joey11602 <joey35475@outlook.com>
2024-02-01 23:05:30 -0600

    Changed the Create and Edit pages of Blog Author.

--- TheatreCMS3/Areas/Blog/Models/BlogAuthor.cs
+++ TheatreCMS3/Areas/Blog/Models/BlogAuthor.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace TheatreCMS3.Areas.Blog.Models
 {
@@ -10,7 +7,7 @@ namespace TheatreCMS3.Areas.Blog.Models
         public int BlogAuthorId { get; set; }
         public string Name { get; set; }
         public string Bio { get; set; }
-        public Nullable<DateTime> Joined { get; set; }
-        public Nullable<DateTime> Left { get; set; } 
+        public DateTime Joined { get; set; }
+        public Nullable<DateTime> Left { get; set; }
     }
 }
