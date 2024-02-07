817f410b00cb25b1da7ed456bc4ae8b9d173780c
Zsuzsanna Mangu <zsuzsannamangu@gmail.com>
2024-02-07 15:34:51 -0800

    Adds seed method for HistoryManager and creates HistoryManager user role.

--- TheatreCMS3/Migrations/Configuration.cs
+++ TheatreCMS3/Migrations/Configuration.cs
@@ -9,6 +9,7 @@
     using Microsoft.AspNet.Identity.EntityFramework;
     using TheatreCMS3.Models;
     using TheatreCMS3.Areas.Prod.Models;
+    using TheatreCMS3.Areas.Rent.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<TheatreCMS3.Models.ApplicationDbContext>
     {
@@ -21,7 +22,7 @@
         protected override void Seed(ApplicationDbContext context)
         {
             EventPlanner.SeedEventPlanner(context);
-
+            HistoryManager.SeedHistoryManager(context);
         }
 
     }
