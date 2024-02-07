f6996fa55ea5eb59d65c0120c924c99c156cb53d
Jiaha0-Zhang <xihd2017@gmail.com>
2024-02-06 16:11:43 -0800

    Event Planner User Part 2: Seed EventPlanner

--- TheatreCMS3/Migrations/Configuration.cs
+++ TheatreCMS3/Migrations/Configuration.cs
@@ -8,7 +8,7 @@
     using Microsoft.AspNet.Identity;
     using Microsoft.AspNet.Identity.EntityFramework;
     using TheatreCMS3.Models;
-
+    using TheatreCMS3.Areas.Prod.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<TheatreCMS3.Models.ApplicationDbContext>
     {
@@ -19,7 +19,31 @@
 
         }
 
-       
+        protected override void Seed(ApplicationDbContext context)
+        {
+            context.SeedData();
+            EventPlanner.SeedEventPlanner(context);
+
+            ////create a user role called "EventPlanner" and assign it to the EventPlanner being seeded.
+            //var eventPlanner = new IdentityRole("EventPlaner");
+            //context.Roles.AddOrUpdate(r => r.Name, eventPlanner);
+            //context.SaveChanges();
+
+            //EventPlanner.SeedEventPlanner(context); //call EventPlanner Method
+
+
+            ////Assign EventPlanner to the "EventPlanner" role
+            //var eventPlannerUser = context.Users.FirstOrDefault(u => u.UserName == "Joel");
+            //if (eventPlannerUser != null)
+            //{
+            //    var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            //    userManager.AddToRole(eventPlannerUser.Id, "EventPlanner");
+            //}
+            //base.Seed(context);
+
+        }
+
+
     }
 
 }
