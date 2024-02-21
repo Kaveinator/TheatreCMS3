2a284ec7f6955c69810c9964b198c8f0b0382419
Joel Johnson <82834522+joelmindset@users.noreply.github.com>
2024-02-21 16:06:13 -0700

    Revert "added css for the create.cshtml page on the site.css file and JS to the Rent.js"
    This reverts commit 08be56be4a6c2a68d739db71ef0153339652dcc7.

--- dbo.Table.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[RentHistory]
-(
-	[RentalHistoryId] INT NOT NULL PRIMARY KEY, 
-    [RentalDamaged] BIT NULL, 
-    [DamagesIncurred] NCHAR(10) NULL, 
-    [Rental] NCHAR(10) NULL
-)
