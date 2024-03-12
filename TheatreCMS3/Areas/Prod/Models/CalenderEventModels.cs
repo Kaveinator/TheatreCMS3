515a20b4ce83419d3c0bd493a7d055ef1a90fa71
judahkirkwood <judahkirkwood@gmail.com>
2024-03-12 11:42:53 -0700

    CalenderEvent

---
+++ TheatreCMS3/Areas/Prod/Models/CalenderEventModels.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace TheatreCMS3.Areas.Prod.Models
+{
+    public class CalenderEventModels
+    {
+        //[Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public bool AllDay { get; set; }
+        public int TicketsAvailable { get; set; }
+        public bool IsProduction { get; set; }
+    }
+}
+
