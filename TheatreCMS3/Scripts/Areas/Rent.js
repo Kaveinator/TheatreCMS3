1f6405a84033d0a89c3c8b458c7517214c9e8025
lilyheym <138812924+lilyheym@users.noreply.github.com>
2024-03-13 11:23:23 -0700

    not finished

--- TheatreCMS3/Scripts/Areas/Rent.js
+++ TheatreCMS3/Scripts/Areas/Rent.js
@@ -1,1 +1,14 @@
-﻿
+﻿alert("Hello World!");
+//$document.ready(function () {
+//    // Attach change event handler to the checkbox
+//    $('#rentalDamagedCheckbox').change(function () {
+//        // Check if checkbox is checked
+//        if ($(this).is(':checked')) {
+//            // If checked, set text of "Damages Incurred" to "Notes"
+//            $('#damagesIncurredTextbox').val('Notes');
+//        } else {
+//            // If not checked, set the text of "Damages Incurred" textbox to 'Default Text'
+//            $('#damagesIncurred.Textbox').val('Damages Incurred');
+//        }
+//    });
+//});
