c994bb70449c49c4595a573e13e9746edbaeed7d
lilyheym <138812924+lilyheym@users.noreply.github.com>
2024-03-13 11:45:38 -0700

    Works!

--- TheatreCMS3/Scripts/Areas/Rent.js
+++ TheatreCMS3/Scripts/Areas/Rent.js
@@ -1,14 +1,13 @@
-﻿alert("Hello World!");
-//$document.ready(function () {
-//    // Attach change event handler to the checkbox
-//    $('#rentalDamagedCheckbox').change(function () {
-//        // Check if checkbox is checked
-//        if ($(this).is(':checked')) {
-//            // If checked, set text of "Damages Incurred" to "Notes"
-//            $('#damagesIncurredTextbox').val('Notes');
-//        } else {
-//            // If not checked, set the text of "Damages Incurred" textbox to 'Default Text'
-//            $('#damagesIncurred.Textbox').val('Damages Incurred');
-//        }
-//    });
-//});
+﻿$(document).ready(function () {
+    // Attach change event handler to the checkbox
+    $('#rentalDamagedCheckbox').change(function () {
+        // Check if checkbox is checked
+        if ($(this).is(':checked')) {
+            // If checked, change the label text to "Notes"
+            $('label[for="DamagesIncurred"]').text('Notes');
+        } else {
+            // If not checked, restore the label text to 'Damages Incurred'
+            $('label[for="DamagesIncurred"]').text('Damages Incurred');
+        }
+    });
+});
