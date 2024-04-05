9735fa1be86cb5e747db351e5a9f70478f4a8607
jokel1973 <jordan.kelley@gmail.com>
2024-04-05 16:26:50 -0700

    BlogAuthor

---
+++ TheatreCMS3/Areas/Blog/Models/BlogAuthor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheatreCMS3.Models
+{
+    public class BlogAuthor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Bio { get; set; }
+        public int Joined { get; set; }
+        public int Left { get; set; }
+
+    }
+}
