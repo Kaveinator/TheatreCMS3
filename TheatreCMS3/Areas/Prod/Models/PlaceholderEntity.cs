a59769314afb833238bf5274008f3492a020fbb0
Kaveman <55042475+Kaveinator@users.noreply.github.com>
2024-04-24 14:09:45 -0700

    Add Dummy PlaceholderEntity

---
+++ TheatreCMS3/Areas/Prod/Models/PlaceholderEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TheatreCMS3.Areas.Prod.Models {
+    public class PlaceholderEntity {
+        [Key] public int InstanceId { get; set; }
+        [Required] public string Value { get; set; }
+    }
+}
--- TheatreCMS3/Models/IdentityModels.cs
+++ TheatreCMS3/Models/IdentityModels.cs
@@ -47,5 +47,7 @@ namespace TheatreCMS3.Models
         public System.Data.Entity.DbSet<TheatreCMS3.Areas.Rent.Models.RentalHistory> RentalHistories { get; set; }
 
         public System.Data.Entity.DbSet<TheatreCMS3.Areas.Prod.Models.CalendarEvent> CalendarEvents { get; set; }
+
+        public System.Data.Entity.DbSet<TheatreCMS3.Areas.Prod.Models.PlaceholderEntity> PlaceholderEntities { get; set; }
     }
 }
