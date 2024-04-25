545448b8cc8f0b91e973cb88c648eedb255cc284
Kaveman <55042475+Kaveinator@users.noreply.github.com>
2024-04-25 10:11:48 -0700

    Create EventPlanner.cs

---
+++ TheatreCMS3/Areas/Prod/Models/EventPlanner.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TheatreCMS3.Models;
+
+namespace TheatreCMS3.Areas.Prod.Models {
+    public class EventPlanner : ApplicationUser {
+        public DateTime PlannerStartDate { get; set; }
+    }
+}
