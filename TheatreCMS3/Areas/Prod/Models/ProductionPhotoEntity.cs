57b0b9cda68c3b09afe30904dfb4ae01e988c778
Kaveman <55042475+Kaveinator@users.noreply.github.com>
2024-04-30 15:31:21 -0700

    Add ProductionPhotoEntity.cs

---
+++ TheatreCMS3/Areas/Prod/Models/ProductionPhotoEntity.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheatreCMS3.Areas.Prod.Models {
+    public class ProductionPhotoEntity {
+        [Key] public long ProPhotoId { get; set; }
+        public byte[] PhotoFile { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
