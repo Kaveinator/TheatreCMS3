0bbf3fd4148cc3bf61c4085cee549e4c087dff2d
Kaveman <55042475+Kaveinator@users.noreply.github.com>
2024-04-30 13:25:57 -0700

    Add EventAuthorize and bind to CalendarEventsController

---
+++ TheatreCMS3/Areas/Prod/Data/EventAuthorizeAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TheatreCMS3.Areas.Prod.Models;
+
+namespace TheatreCMS3.Areas.Prod.Data {
+    public class EventAuthorizeAttribute : AuthorizeAttribute {
+        public EventAuthorizeAttribute() {
+            Roles = nameof(EventPlanner);
+        }
+
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext) {
+            filterContext.Result = new ViewResult {
+                ViewName = "AccessDenied",
+            };
+        }
+    }
+}
