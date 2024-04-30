d8dc1a248d23f127f75fb9594a099f22cab1a1fb
Kaveman <55042475+Kaveinator@users.noreply.github.com>
2024-04-30 15:35:55 -0700

    Hide PhotoFile Field in ProductionPhotoEntity

--- TheatreCMS3/Areas/Prod/Models/ProductionPhotoEntity.cs
+++ TheatreCMS3/Areas/Prod/Models/ProductionPhotoEntity.cs
@@ -3,7 +3,7 @@
 namespace TheatreCMS3.Areas.Prod.Models {
     public class ProductionPhotoEntity {
         [Key] public long ProPhotoId { get; set; }
-        public byte[] PhotoFile { get; set; }
+        //public byte[] PhotoFile { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
     }
